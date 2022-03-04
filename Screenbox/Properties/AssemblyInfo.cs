8e7214d1600fdaa6d512439326a5954508baac7d
Tung Huynh <tung75605@gmail.com>
2022-03-04 08:55:02 -0800

    target windows 11

--- Screenbox/Properties/AssemblyInfo.cs
+++ Screenbox/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Screenbox")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © Tung Huynh 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
