674b4ccd1881907c076df989bc6d81579a69ded0
Tung Huynh <tung75605@gmail.com>
2022-04-27 02:29:41 -0700

    clean up MediaHandle

--- Screenbox/Core/MediaHandle.cs
+++ Screenbox/Core/MediaHandle.cs
@@ -1,11 +1,7 @@
 ﻿#nullable enable
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Windows.Storage;
 using LibVLCSharp.Shared;
 using Microsoft.Win32.SafeHandles;
 
@@ -18,32 +14,19 @@ namespace Screenbox.Core
         public SafeFileHandle? FileHandle { get; set; }
         public Stream? Stream { get; set; }
         public StreamMediaInput? StreamInput { get; set; }
-        public List<StorageFile> SubtitleFiles { get; }
 
         public MediaHandle(Media media, Uri uri)
         {
-            SubtitleFiles = new List<StorageFile>();
             Media = media;
             Uri = uri;
         }
 
-        public async void Dispose()
+        public void Dispose()
         {
             Media.Dispose();
             StreamInput?.Dispose();
             Stream?.Dispose();
             FileHandle?.Dispose();
-
-            if (SubtitleFiles.Count == 0) return;
-            try
-            {
-                await Task.WhenAll(SubtitleFiles.Select(f => f.DeleteAsync(StorageDeleteOption.PermanentDelete).AsTask())
-                    .ToArray());
-            }
-            catch (Exception)
-            {
-                // pass
-            }
         }
     }
 }
