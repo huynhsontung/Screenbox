31b3c6b76b020a0f3d3da69fbb38974ac34fe581
Tung Huynh <tung75605@gmail.com>
2022-04-27 02:29:42 -0700

    create TimeDisplay

--- Screenbox/Controls/SeekBar.xaml.cs
+++ Screenbox/Controls/SeekBar.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Microsoft.Extensions.DependencyInjection;
 using Screenbox.ViewModels;
@@ -16,33 +15,11 @@ namespace Screenbox.Controls
             typeof(bool),
             typeof(SeekBar),
             new PropertyMetadata(false));
-        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register(
-            nameof(Time),
-            typeof(double),
-            typeof(SeekBar),
-            new PropertyMetadata(0d));
-        public static readonly DependencyProperty LengthProperty = DependencyProperty.Register(
-            nameof(Length),
-            typeof(double),
-            typeof(SeekBar),
-            new PropertyMetadata(0d));
 
         public bool ProgressOnly
         {
             get => (bool)GetValue(ProgressOnlyProperty);
             set => SetValue(ProgressOnlyProperty, value);
-        }
-
-        public double Time
-        {
-            get => (double)GetValue(TimeProperty);
-            private set => SetValue(TimeProperty, value);
-        }
-
-        public double Length
-        {
-            get => (double)GetValue(LengthProperty);
-            private set => SetValue(LengthProperty, value);
         }
 
         internal SeekBarViewModel ViewModel => (SeekBarViewModel)DataContext;
@@ -52,16 +29,6 @@ namespace Screenbox.Controls
             this.InitializeComponent();
             DataContext = App.Services.GetRequiredService<SeekBarViewModel>();
             RegisterSeekBarPointerHandlers();
-            GenerateBindings();
-        }
-
-        private void GenerateBindings()
-        {
-            Binding timeBinding = new() { Path = new PropertyPath("Time") };
-            SetBinding(TimeProperty, timeBinding);
-
-            Binding lengthBinding = new() { Path = new PropertyPath("Length") };
-            SetBinding(LengthProperty, lengthBinding);
         }
 
         private void RegisterSeekBarPointerHandlers()
---
+++ Screenbox/Controls/TimeDisplay.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Screenbox.Converters;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Screenbox.Controls
+{
+    public sealed partial class TimeDisplay : UserControl
+    {
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register(
+            nameof(Time),
+            typeof(double),
+            typeof(TimeDisplay),
+            new PropertyMetadata(0d));
+        public static readonly DependencyProperty LengthProperty = DependencyProperty.Register(
+            nameof(Length),
+            typeof(double),
+            typeof(TimeDisplay),
+            new PropertyMetadata(0d));
+
+        public double Time
+        {
+            get => (double)GetValue(TimeProperty);
+            set => SetValue(TimeProperty, value);
+        }
+
+        public double Length
+        {
+            get => (double)GetValue(LengthProperty);
+            set => SetValue(LengthProperty, value);
+        }
+
+        private bool _showRemaining;
+
+        public TimeDisplay()
+        {
+            this.InitializeComponent();
+        }
+
+        private string GetRemainingTime(double currentTime) => HumanizedDurationConverter.Convert(currentTime - Length);
+
+        private void TimeDisplay_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            _showRemaining = !_showRemaining;
+            if (_showRemaining)
+            {
+                RemainingText.Visibility = Visibility.Visible;
+                LengthText.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                RemainingText.Visibility = Visibility.Collapsed;
+                LengthText.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
