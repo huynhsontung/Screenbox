f660a046f8e79e983f696629b4943dc03f0aafa7
Tung Huynh <tung75605@gmail.com>
2022-05-11 23:41:13 -0700

    flyout improvements

--- Screenbox/Controls/AudioTrackSubtitlePicker.xaml.cs
+++ Screenbox/Controls/AudioTrackSubtitlePicker.xaml.cs
@@ -8,20 +8,12 @@ namespace Screenbox.Controls
 {
     public sealed partial class AudioTrackSubtitlePicker : UserControl
     {
-        private AudioTrackSubtitleViewModel ViewModel => (AudioTrackSubtitleViewModel)DataContext;
+        internal AudioTrackSubtitleViewModel ViewModel => (AudioTrackSubtitleViewModel)DataContext;
 
         public AudioTrackSubtitlePicker()
         {
             this.InitializeComponent();
             DataContext = App.Services.GetRequiredService<AudioTrackSubtitleViewModel>();
-        }
-
-        public static Flyout GetFlyout()
-        {
-            AudioTrackSubtitlePicker control = new();
-            Flyout flyout = new() { Content = control };
-            flyout.Opening += (_, _) => control.ViewModel.OnAudioCaptionFlyoutOpening();
-            return flyout;
         }
     }
 }
