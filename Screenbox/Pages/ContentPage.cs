dae9cb04b2135072c10a0f1dac3bcea3121b013a
Tung Huynh <tung75605@gmail.com>
2022-05-16 02:06:55 -0700

    add PlayQueuePage

---
+++ Screenbox/Pages/ContentPage.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Screenbox.Pages
+{
+    public class ContentPage : Page
+    {
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+            nameof(Header),
+            typeof(object),
+            typeof(ContentPage),
+            new PropertyMetadata(null));
+
+        public object? Header
+        {
+            get => GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        public bool CanGoBack { get; protected set; }
+
+        public virtual void GoBack()
+        {
+        }
+    }
+}
--- Screenbox/Pages/PlayQueuePage.xaml.cs
+++ Screenbox/Pages/PlayQueuePage.xaml.cs
@@ -20,7 +20,7 @@ namespace Screenbox.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class PlayQueuePage : Page
+    public sealed partial class PlayQueuePage : ContentPage
     {
         public PlayQueuePage()
         {
