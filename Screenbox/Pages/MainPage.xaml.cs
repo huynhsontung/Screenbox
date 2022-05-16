744770e4054768c22633ac9ca1dcacb516069cf8
Tung Huynh <tung75605@gmail.com>
2022-05-16 02:04:27 -0700

    fix Header not set when going back on MainPage

--- Screenbox/Pages/MainPage.xaml.cs
+++ Screenbox/Pages/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using Screenbox.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
@@ -150,7 +151,16 @@ namespace Screenbox.Pages
             }
             else if (ContentFrame.SourcePageType != null)
             {
-                NavView.Header = ((muxc.NavigationViewItem)NavView.SelectedItem)?.Content?.ToString();
+                KeyValuePair<string, Type> item = _pages.FirstOrDefault(p => p.Value == e.SourcePageType);
+
+                muxc.NavigationViewItem selectedItem = NavView.MenuItems
+                    .OfType<muxc.NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Key));
+                
+                NavView.SelectedItem = selectedItem;
+                NavView.Header = ContentFrame.Content is ContentPage page
+                    ? page.Header
+                    : selectedItem.Content?.ToString();
             }
         }
 
