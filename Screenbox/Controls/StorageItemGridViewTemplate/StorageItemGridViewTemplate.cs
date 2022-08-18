2fea2119064d44000583b041a674806e225f9a60
Tung Huynh <tung75605@gmail.com>
2022-08-19 00:08:00 +0700

    turn FolderViewPage data template into a StaticResource for future reuse

---
+++ Screenbox/Controls/StorageItemGridViewTemplate/StorageItemGridViewTemplate.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml;
+
+namespace Screenbox.Controls
+{
+    // Code-behind class for {x:Bind}
+    public sealed partial class StorageItemGridViewTemplate : ResourceDictionary
+    {
+        public StorageItemGridViewTemplate()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
