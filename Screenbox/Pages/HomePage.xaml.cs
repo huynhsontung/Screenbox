f2f1b3da4d5928152fc31f74308e88605f812c87
Tung Huynh <tung75605@gmail.com>
2022-09-06 06:25:42 +0800

    update HomePage layout

--- Screenbox/Pages/HomePage.xaml.cs
+++ Screenbox/Pages/HomePage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 using Microsoft.Extensions.DependencyInjection;
 using Screenbox.ViewModels;
 
@@ -10,7 +9,7 @@ namespace Screenbox.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class HomePage : Page
+    public sealed partial class HomePage : ContentPage
     {
         internal HomePageViewModel ViewModel => (HomePageViewModel)DataContext;
 
