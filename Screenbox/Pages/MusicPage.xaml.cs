50fab4e466d71074e7ed17aae43219f59abb332c
Tung Huynh <tung75605@gmail.com>
2022-12-17 01:33:24 -0800

    feat: add context menu for MusicPage

--- Screenbox/Pages/MusicPage.xaml.cs
+++ Screenbox/Pages/MusicPage.xaml.cs
@@ -2,6 +2,8 @@
 
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Extensions.DependencyInjection;
 using Screenbox.ViewModels;
@@ -28,6 +30,36 @@ namespace Screenbox.Pages
             VisualStateManager.GoToState(this, "Fetching", true);
             await ViewModel.FetchSongsAsync();
             VisualStateManager.GoToState(this, ViewModel.GroupedSongs?.Count > 0 ? "Normal" : "NoContent", true);
+        }
+
+        private void SongListView_OnRightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            if (ItemFlyout.Items == null) return;
+            if (e.OriginalSource is FrameworkElement { DataContext: MediaViewModel media } element)
+            {
+                foreach (MenuFlyoutItemBase itemBase in ItemFlyout.Items)
+                {
+                    itemBase.DataContext = media;
+                }
+
+                ItemFlyout.ShowAt(element, e.GetPosition(element));
+                e.Handled = true;
+            }
+        }
+
+        private void SongListView_OnContextRequested(UIElement sender, ContextRequestedEventArgs args)
+        {
+            if (ItemFlyout.Items == null) return;
+            if (args.OriginalSource is SelectorItem item)
+            {
+                foreach (MenuFlyoutItemBase itemBase in ItemFlyout.Items)
+                {
+                    itemBase.DataContext = item.Content;
+                }
+
+                ItemFlyout.ShowAt(item);
+                args.Handled = true;
+            }
         }
     }
 }
