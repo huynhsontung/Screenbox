33d73ff1085fcb860b6a5d061b427b5b9256e19d
Tung Huynh <tung75605@gmail.com>
2022-12-25 22:29:30 -0800

    style: readjust spacing for settings page

--- Screenbox/Pages/SettingsPage.xaml.cs
+++ Screenbox/Pages/SettingsPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 using Microsoft.Extensions.DependencyInjection;
 using Screenbox.ViewModels;
 
@@ -17,6 +21,18 @@ namespace Screenbox.Pages
         {
             this.InitializeComponent();
             DataContext = App.Services.GetRequiredService<SettingsPageViewModel>();
+        }
+
+        private void ContentRoot_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            StackPanel panel = (StackPanel)sender;
+            ButtonBase? settingsCard = panel.Children.OfType<ButtonBase>().FirstOrDefault();
+            if (settingsCard == null) return;
+            IEnumerable<TextBlock> sectionHeaders = panel.Children.OfType<TextBlock>();
+            foreach (TextBlock sectionHeader in sectionHeaders)
+            {
+                sectionHeader.Width = settingsCard.ActualWidth;
+            }
         }
     }
 }
