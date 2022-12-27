632f3174863ebd22e0ffd66258b310948dbaebdb
Tung Huynh <tung75605@gmail.com>
2022-12-26 23:07:01 -0800

    ci: update package manifest for store upload

---
+++ scripts/update-manifest.ps1
@@ -0,0 +1,9 @@
+# Update package manifest for store upload
+$ProjectPath = "$PSScriptRoot\..\Screenbox"
+$ManifestPath = "$ProjectPath\Package.appxmanifest"
+[xml]$xmlDoc = Get-Content $ManifestPath
+$xmlDoc.Package.Identity.Name="18496Starpine.Screenbox"
+$xmlDoc.Package.Identity.Publisher="CN=ABCDF790-DBE4-48F7-8204-32FCB69ADF9C"
+$xmlDoc.Package.Properties.DisplayName="Screenbox"
+$xmlDoc.Package.Applications.Application.VisualElements.DisplayName="Screenbox"
+$xmlDoc.Save($ManifestPath)
