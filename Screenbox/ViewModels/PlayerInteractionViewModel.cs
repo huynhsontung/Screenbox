15057f120dbfad292331a8bd46e6d29847f9a43b
Tung Huynh <tung75605@gmail.com>
2023-01-07 16:41:33 -0800

    fix: missing nullable directive

--- Screenbox/ViewModels/PlayerInteractionViewModel.cs
+++ Screenbox/ViewModels/PlayerInteractionViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
 using Windows.Media.Playback;
 using Windows.System;
--- Screenbox/ViewModels/SettingsPageViewModel.cs
+++ Screenbox/ViewModels/SettingsPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.ObjectModel;
 using Windows.Storage;
 using CommunityToolkit.Mvvm.ComponentModel;
