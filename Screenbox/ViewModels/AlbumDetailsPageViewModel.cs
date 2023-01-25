b412875c6caa188cb65413bf99f72e306010893a
Tung Huynh <tung75605@gmail.com>
2023-01-25 00:15:05 -0800

    feat: implement play command for album details page

--- Screenbox/ViewModels/AlbumDetailsPageViewModel.cs
+++ Screenbox/ViewModels/AlbumDetailsPageViewModel.cs
@@ -1,14 +1,61 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Windows.Storage.FileProperties;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Toolkit.Uwp.UI;
+using Screenbox.Core.Messages;
+using Screenbox.Core;
 
 namespace Screenbox.ViewModels
 {
     internal sealed partial class AlbumDetailsPageViewModel : ObservableRecipient
     {
         [ObservableProperty] private AlbumViewModel _source = null!;
+
+        public AdvancedCollectionView SortedItems { get; }
+
+        private List<MediaViewModel>? _itemList;
+
+        private class TrackNumberComparer : IComparer
+        {
+            public int Compare(object x, object y)
+            {
+                MusicProperties? m1 = x as MusicProperties;
+                MusicProperties? m2 = y as MusicProperties;
+                uint t1 = m1?.TrackNumber ?? uint.MaxValue;
+                uint t2 = m2?.TrackNumber ?? uint.MaxValue;
+                if (t1 == t2) return 0;
+                return t1 > t2 ? 1 : -1;
+            }
+        }
+
+        public AlbumDetailsPageViewModel()
+        {
+            SortedItems = new AdvancedCollectionView();
+            SortedItems.SortDescriptions.Add(new SortDescription(nameof(MediaViewModel.MusicProperties),
+                SortDirection.Ascending, new TrackNumberComparer()));
+        }
+
+        partial void OnSourceChanged(AlbumViewModel value)
+        {
+            SortedItems.Source = value.RelatedSongs;
+        }
+
+        [RelayCommand]
+        private void Play(MediaViewModel item)
+        {
+            _itemList ??= SortedItems.OfType<MediaViewModel>().ToList();
+            PlaylistInfo playlist = Messenger.Send(new PlaylistRequestMessage());
+            if (playlist.Playlist.Count != _itemList.Count || playlist.LastUpdate != _itemList)
+            {
+                Messenger.Send(new ClearPlaylistMessage());
+                Messenger.Send(new QueuePlaylistMessage(_itemList, false));
+            }
+
+            Messenger.Send(new PlayMediaMessage(item, true));
+        }
     }
 }
