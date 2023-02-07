2e819f77d53b5209182c4e83d991d9422f685466
Tung Huynh <tung75605@gmail.com>
2023-02-06 18:01:51 -0800

    feat: add behavior for showing context menu on list items

---
+++ Screenbox/Controls/Interactions/ListViewContextTriggerBehavior.cs
@@ -0,0 +1,90 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+using Microsoft.Toolkit.Uwp.UI;
+using Microsoft.Xaml.Interactivity;
+
+namespace Screenbox.Controls.Interactions
+{
+    internal class ListViewContextTriggerBehavior : Trigger<ListViewBase>
+    {
+        public static readonly DependencyProperty FlyoutProperty = DependencyProperty.Register(
+            nameof(Flyout),
+            typeof(FlyoutBase),
+            typeof(ListViewContextTriggerBehavior),
+            new PropertyMetadata(null));
+
+        public FlyoutBase? Flyout
+        {
+            get => (FlyoutBase?)GetValue(FlyoutProperty);
+            set => SetValue(FlyoutProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.ContextRequested += OnContextRequested;
+            AssociatedObject.RightTapped += OnRightTapped;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.ContextRequested -= OnContextRequested;
+            AssociatedObject.RightTapped -= OnRightTapped;
+        }
+
+        private void OnContextRequested(UIElement sender, ContextRequestedEventArgs args)
+        {
+            if (args.OriginalSource is not SelectorItem element) return;
+            Interaction.ExecuteActions(AssociatedObject, Actions, element.Content);
+            if (Flyout == null) return;
+            if (Flyout is MenuFlyout { Items: { } } menuFlyout)
+            {
+                SetMenuFlyoutDataContext(menuFlyout.Items, element.Content);
+            }
+
+            Flyout.ShowAt(element);
+            args.Handled = true;
+        }
+
+        private void OnRightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            if (e.OriginalSource is not FrameworkElement element ||
+                element.FindAscendantOrSelf<SelectorItem>() == null) return;
+            Interaction.ExecuteActions(AssociatedObject, Actions, element.DataContext);
+            if (Flyout == null) return;
+            if (Flyout is MenuFlyout { Items: { } } menuFlyout)
+            {
+                SetMenuFlyoutDataContext(menuFlyout.Items, element.DataContext);
+                menuFlyout.ShowAt(element, e.GetPosition(element));
+            }
+            else
+            {
+                Flyout.ShowAt(element);
+            }
+
+            e.Handled = true;
+        }
+
+        private void SetMenuFlyoutDataContext(IList<MenuFlyoutItemBase> items, object? dataContext)
+        {
+            List<MenuFlyoutItemBase> menuFlyoutItems = new(items);
+            for (int i = 0; i < menuFlyoutItems.Count; i++)
+            {
+                MenuFlyoutItemBase item = menuFlyoutItems[i];
+                item.DataContext = dataContext;
+                if (item is MenuFlyoutSubItem { Items: { } } subItem)
+                {
+                    menuFlyoutItems.AddRange(subItem.Items);
+                }
+            }
+        }
+    }
+}
