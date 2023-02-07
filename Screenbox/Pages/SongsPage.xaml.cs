96b927c68f5b027b2db64074ea50b705004cee8e
Tung Huynh <tung75605@gmail.com>
2023-02-06 20:44:03 -0800

    refactor: songs page should also use the behavior

--- Screenbox/Pages/SongsPage.xaml.cs
+++ Screenbox/Pages/SongsPage.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Input;
+﻿using Windows.UI.Xaml.Controls;
 using Microsoft.Extensions.DependencyInjection;
 using Screenbox.ViewModels;
 
@@ -23,36 +20,6 @@ namespace Screenbox.Pages
             this.InitializeComponent();
             DataContext = App.Services.GetRequiredService<MusicPageViewModel>();
             Common = App.Services.GetRequiredService<CommonViewModel>();
-        }
-
-        private void SongListView_OnRightTapped(object sender, RightTappedRoutedEventArgs e)
-        {
-            if (ItemFlyout.Items == null) return;
-            if (e.OriginalSource is FrameworkElement { DataContext: MediaViewModel media } element)
-            {
-                foreach (MenuFlyoutItemBase itemBase in ItemFlyout.Items)
-                {
-                    itemBase.DataContext = media;
-                }
-
-                ItemFlyout.ShowAt(element, e.GetPosition(element));
-                e.Handled = true;
-            }
-        }
-
-        private void SongListView_OnContextRequested(UIElement sender, ContextRequestedEventArgs args)
-        {
-            if (ItemFlyout.Items == null) return;
-            if (args.OriginalSource is SelectorItem item)
-            {
-                foreach (MenuFlyoutItemBase itemBase in ItemFlyout.Items)
-                {
-                    itemBase.DataContext = item.Content;
-                }
-
-                ItemFlyout.ShowAt(item);
-                args.Handled = true;
-            }
         }
     }
 }
