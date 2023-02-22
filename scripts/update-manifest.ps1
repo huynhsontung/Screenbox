488fdb0c20010f3af22ae592332eaaa10a7500fd
Tung Huynh <tung75605@gmail.com>
2023-02-22 01:18:04 -0800

    ci: extract version from tag and update manifest

--- scripts/update-manifest.ps1
+++ scripts/update-manifest.ps1
@@ -1,3 +1,10 @@
+[CmdletBinding()]
+param (
+  [Parameter(Mandatory=$false)]
+  [string]
+  $Version
+)
+
 # Update package manifest for store upload
 $ProjectPath = "$PSScriptRoot\..\Screenbox"
 $ManifestPath = "$ProjectPath\Package.appxmanifest"
@@ -6,4 +13,9 @@ $xmlDoc.Package.Identity.Name="18496Starpine.Screenbox"
 $xmlDoc.Package.Identity.Publisher="CN=ABCDF790-DBE4-48F7-8204-32FCB69ADF9C"
 $xmlDoc.Package.Properties.DisplayName="Screenbox"
 $xmlDoc.Package.Applications.Application.VisualElements.DisplayName="Screenbox"
+if ($Version -match '^(\d+.\d+.\d+)$') {
+  $Version = $Version + ".0"
+  $xmlDoc.Package.Identity.Version = $Version
+}
+
 $xmlDoc.Save($ManifestPath)
