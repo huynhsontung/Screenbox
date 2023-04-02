d68dbac75dd7dbcbab5feb594a640a80109d18b2
Tung Huynh <tung75605@gmail.com>
2023-04-02 16:38:07 -0700

    enforce property return type

--- Screenbox/Services/ResourceService.cs
+++ Screenbox/Services/ResourceService.cs
@@ -19,10 +19,10 @@ namespace Screenbox.Services
                     Guard.HasSizeGreaterThanOrEqualTo(parameters, 1);
                     return Resources.VolumeChangeStatusMessage(Convert.ToDouble(parameters[0]));
                 default:
-                    string valueName = Enum.GetName(typeof(ResourceName), name) ??
+                    string resourceName = Enum.GetName(typeof(ResourceName), name) ??
                                        throw new ArgumentOutOfRangeException(nameof(name), name, null);
-                    return typeof(Resources).GetProperty(valueName)?.GetValue(null) as string ??
-                           throw new ArgumentOutOfRangeException(nameof(valueName), valueName, "Incorrect resource name");
+                    return typeof(Resources).GetProperty(resourceName, typeof(string))?.GetValue(null) as string ??
+                           throw new ArgumentOutOfRangeException(nameof(resourceName), resourceName, "Invalid resource name");
             }
         }
     }
