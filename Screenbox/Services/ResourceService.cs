9b071341cb0a3cefc94848ab996246bc380825d6
Tung Huynh <tung75605@gmail.com>
2023-04-02 16:34:42 -0700

    use reflection to simplify ResourceService

--- Screenbox/Services/ResourceService.cs
+++ Screenbox/Services/ResourceService.cs
@@ -12,74 +12,17 @@ namespace Screenbox.Services
         {
             switch (name)
             {
-                case ResourceName.PropertyTitle:
-                    return Resources.PropertyTitle;
-                case ResourceName.PropertySubtitle:
-                    return Resources.PropertySubtitle;
-                case ResourceName.PropertyYear:
-                    return Resources.PropertyYear;
-                case ResourceName.PropertyProducers:
-                    return Resources.PropertyProducers;
-                case ResourceName.PropertyWriters:
-                    return Resources.PropertyWriters;
-                case ResourceName.PropertyLength:
-                    return Resources.PropertyLength;
-                case ResourceName.PropertyResolution:
-                    return Resources.PropertyResolution;
-                case ResourceName.PropertyBitRate:
-                    return Resources.PropertyBitRate;
-                case ResourceName.PropertyContributingArtists:
-                    return Resources.PropertyContributingArtists;
-                case ResourceName.PropertyAlbum:
-                    return Resources.PropertyAlbum;
-                case ResourceName.PropertyAlbumArtist:
-                    return Resources.PropertyAlbumArtist;
-                case ResourceName.PropertyGenre:
-                    return Resources.PropertyGenre;
-                case ResourceName.PropertyTrack:
-                    return Resources.PropertyTrack;
-                case ResourceName.PropertyFileType:
-                    return Resources.PropertyFileType;
-                case ResourceName.PropertyContentType:
-                    return Resources.PropertyContentType;
-                case ResourceName.PropertySize:
-                    return Resources.PropertySize;
-                case ResourceName.PropertyLastModified:
-                    return Resources.PropertyLastModified;
-                case ResourceName.UnknownArtist:
-                    return Resources.UnknownArtist;
-                case ResourceName.UnknownAlbum:
-                    return Resources.UnknownAlbum;
-                case ResourceName.Disable:
-                    return Resources.Disable;
-                case ResourceName.FailedToLoadSubtitleNotificationTitle:
-                    return Resources.FailedToLoadSubtitleNotificationTitle;
-                case ResourceName.FailedToSaveFrameNotificationTitle:
-                    return Resources.FailedToSaveFrameNotificationTitle;
-                case ResourceName.FrameSavedNotificationTitle:
-                    return Resources.FrameSavedNotificationTitle;
-                case ResourceName.ResumePositionNotificationTitle:
-                    return Resources.ResumePositionNotificationTitle;
                 case ResourceName.GoToPosition:
                     Guard.HasSizeGreaterThanOrEqualTo(parameters, 1);
                     return Resources.GoToPosition((string)parameters[0]);
                 case ResourceName.VolumeChangeStatusMessage:
                     Guard.HasSizeGreaterThanOrEqualTo(parameters, 1);
                     return Resources.VolumeChangeStatusMessage(Convert.ToDouble(parameters[0]));
-                case ResourceName.AccessDeniedMusicLibraryTitle:
-                    return Resources.AccessDeniedMusicLibraryTitle;
-                case ResourceName.AccessDeniedVideosLibraryTitle:
-                    return Resources.AccessDeniedVideosLibraryTitle;
-                case ResourceName.AccessDeniedPicturesLibraryTitle:
-                    return Resources.AccessDeniedPicturesLibraryTitle;
-                case ResourceName.OpenPrivacySettingsButtonText:
-                    return Resources.OpenPrivacySettingsButtonText;
-                case ResourceName.AccessDeniedMessage:
-                    return Resources.AccessDeniedMessage;
-                case ResourceName.FailedToLoadMediaNotificationTitle:
-                    return Resources.FailedToLoadMediaNotificationTitle;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(name), name, null);
+                    string valueName = Enum.GetName(typeof(ResourceName), name) ??
+                                       throw new ArgumentOutOfRangeException(nameof(name), name, null);
+                    return typeof(Resources).GetProperty(valueName)?.GetValue(null) as string ??
+                           throw new ArgumentOutOfRangeException(nameof(valueName), valueName, "Incorrect resource name");
             }
         }
     }
