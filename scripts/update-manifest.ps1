7974d43403ae82141dd6160735279f2a5440716b
Herbert Persley <United600@users.noreply.github.com>
2023-05-05 15:05:56 +0100

    factoring manifest strings into other resource file

--- scripts/update-manifest.ps1
+++ scripts/update-manifest.ps1
@@ -12,7 +12,7 @@ $ManifestPath = "$ProjectPath\Package.appxmanifest"
 $xmlDoc.Package.Identity.Name="18496Starpine.Screenbox"
 $xmlDoc.Package.Identity.Publisher="CN=ABCDF790-DBE4-48F7-8204-32FCB69ADF9C"
 $xmlDoc.Package.Properties.DisplayName="Screenbox"
-$xmlDoc.Package.Applications.Application.VisualElements.DisplayName="Screenbox"
+$xmlDoc.Package.Applications.Application.VisualElements.DisplayName="ms-resource:ManifestResources/AppDisplayName"
 if ($Version -match '^(\d+.\d+.\d+)$') {
   $Version = $Version + ".0"
   $xmlDoc.Package.Identity.Version = $Version
