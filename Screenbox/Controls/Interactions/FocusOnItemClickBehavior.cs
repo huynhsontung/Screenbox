1573df039b9a12a7e0cd0d2a532571a062ee68d4
Tung Huynh <tung75605@gmail.com>
2023-06-04 16:29:10 -0700

    focus first button on item click for media ListViews

---
+++ Screenbox/Controls/Interactions/FocusOnItemClickBehavior.cs
@@ -0,0 +1,30 @@
+#nullable enable
+
+using Microsoft.Toolkit.Uwp.UI;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Screenbox.Controls.Interactions;
+
+internal class FocusOnItemClickBehavior : Behavior<ListViewBase>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.ItemClick += OnItemClick;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        AssociatedObject.ItemClick -= OnItemClick;
+    }
+
+    private void OnItemClick(object sender, ItemClickEventArgs e)
+    {
+        SelectorItem? item = (SelectorItem?)AssociatedObject.ContainerFromItem(e.ClickedItem);
+        item?.FindDescendant<ButtonBase>()?.Focus(FocusState.Programmatic);
+    }
+}
