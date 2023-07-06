77512afa6004837ec85cc4766d13062498cd3e12
Tung Huynh <tung75605@gmail.com>
2023-07-06 00:23:56 -0700

    use custom slider for seekbar

---
+++ Screenbox/Controls/CustomSlider.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Screenbox.Controls;
+internal class CustomSlider : Slider
+{
+    public static readonly DependencyProperty IsKeyDownEnabledProperty = DependencyProperty.Register(
+        nameof(IsKeyDownEnabled), typeof(bool), typeof(CustomSlider), new PropertyMetadata(true));
+
+    public bool IsKeyDownEnabled
+    {
+        get => (bool)GetValue(IsKeyDownEnabledProperty);
+        set => SetValue(IsKeyDownEnabledProperty, value);
+    }
+
+    protected override void OnKeyDown(KeyRoutedEventArgs e)
+    {
+        if (!IsKeyDownEnabled && !IsFocusEngaged) return;
+        base.OnKeyDown(e);
+    }
+}
