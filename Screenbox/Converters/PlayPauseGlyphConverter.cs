a1f05c9f4c3df870eed2e340804a998cadfd9e47
Tung Huynh <tung75605@gmail.com>
2023-08-13 16:40:45 -0700

    reflect playback state on media list view item

---
+++ Screenbox/Converters/PlayPauseGlyphConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Screenbox.Converters;
+internal class PlayPauseGlyphConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool b)
+        {
+            return b ? "\uE103" : "\uE102";
+        }
+
+        return "\uE102";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
