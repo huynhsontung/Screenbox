c1cb5bd7ea67b3c79dd3e5a4e895b4541881f44b
Tung Huynh <tung75605@gmail.com>
2023-10-14 11:35:39 -0700

    add option to set focus engagement for list view item

--- Screenbox/Controls/Extensions/ListViewExtensions.cs
+++ Screenbox/Controls/Extensions/ListViewExtensions.cs
@@ -11,13 +11,29 @@ namespace Screenbox.Controls.Extensions
             "ItemCornerRadius",
             typeof(CornerRadius),
             typeof(ListViewExtensions),
-            new PropertyMetadata(default(CornerRadius), OnAttachedPropertyChanged));
+            new PropertyMetadata(default(CornerRadius), OnItemCornerRadiusChanged));
 
         public static readonly DependencyProperty ItemMarginProperty = DependencyProperty.RegisterAttached(
             "ItemMargin",
             typeof(Thickness),
             typeof(ListViewExtensions),
-            new PropertyMetadata(default(Thickness), OnAttachedPropertyChanged));
+            new PropertyMetadata(default(Thickness), OnItemMarginChanged));
+
+        public static readonly DependencyProperty IsItemFocusEngagementEnabledProperty = DependencyProperty.RegisterAttached(
+            "IsItemFocusEngagementEnabled",
+            typeof(bool),
+            typeof(ListViewExtensions),
+            new PropertyMetadata(false, OnItemFocusEngagementEnabledChanged));
+
+        public static void SetIsItemFocusEngagementEnabled(DependencyObject element, bool value)
+        {
+            element.SetValue(IsItemFocusEngagementEnabledProperty, value);
+        }
+
+        public static bool GetIsItemFocusEngagementEnabled(DependencyObject element)
+        {
+            return (bool)element.GetValue(IsItemFocusEngagementEnabledProperty);
+        }
 
         public static void SetItemMargin(DependencyObject element, Thickness value)
         {
@@ -39,27 +55,51 @@ namespace Screenbox.Controls.Extensions
             return (CornerRadius)element.GetValue(ItemCornerRadiusProperty);
         }
 
-        private static void OnAttachedPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnItemCornerRadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is not ListViewBase listView) return;
-            listView.ContainerContentChanging -= ListViewOnContainerContentChanging;
-            listView.ContainerContentChanging += ListViewOnContainerContentChanging;
+            listView.ContainerContentChanging -= ChangeItemCornerRadius;
+            listView.ContainerContentChanging += ChangeItemCornerRadius;
         }
 
-        private static void ListViewOnContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        private static void OnItemMarginChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not ListViewBase listView) return;
+            listView.ContainerContentChanging -= ChangeItemMargin;
+            listView.ContainerContentChanging += ChangeItemMargin;
+        }
+
+        private static void OnItemFocusEngagementEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not ListViewBase listView) return;
+            listView.ContainerContentChanging -= ChangeItemFocusEngagementEnabled;
+            listView.ContainerContentChanging += ChangeItemFocusEngagementEnabled;
+        }
+
+        private static void ChangeItemCornerRadius(ListViewBase sender, ContainerContentChangingEventArgs args)
         {
             if (args.Phase > 0 || args.InRecycleQueue) return;
             CornerRadius cornerRadius = GetItemCornerRadius(sender);
-            Thickness margin = GetItemMargin(sender);
             if (args.ItemContainer.FindDescendant<ListViewItemPresenter>() is { } presenter)
             {
                 presenter.CornerRadius = cornerRadius;
             }
+        }
 
+        private static void ChangeItemMargin(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.Phase > 0 || args.InRecycleQueue) return;
+            Thickness margin = GetItemMargin(sender);
             if (args.ItemContainer.FindDescendant<Border>() is { } border)
             {
                 border.Margin = margin;
             }
+        }
+
+        private static void ChangeItemFocusEngagementEnabled(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.Phase > 0 || args.InRecycleQueue) return;
+            args.ItemContainer.IsFocusEngagementEnabled = GetIsItemFocusEngagementEnabled(sender);
         }
     }
 }
