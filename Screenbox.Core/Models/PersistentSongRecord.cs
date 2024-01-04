60af38d2844c8b5938526a1215b1fcb3b2c80bb1
Tung Huynh <tung75605@gmail.com>
2024-01-03 22:53:31 -0800

    add PersistentSongRecord for song caching

---
+++ Screenbox.Core/Models/PersistentSongRecord.cs
@@ -0,0 +1,29 @@
+﻿using ProtoBuf;
+
+namespace Screenbox.Core.Models;
+
+[ProtoContract]
+internal record PersistentSongRecord
+{
+    [ProtoMember(1)]
+    public string Id { get; set; }
+
+    [ProtoMember(2)]
+    public string Title { get; set; }
+
+    [ProtoMember(3)]
+    public string Path { get; set; }
+
+    [ProtoMember(4)]
+    public MusicInfo Properties { get; set; }
+
+    public PersistentSongRecord() { }
+
+    public PersistentSongRecord(string id, string title, string path, MusicInfo properties)
+    {
+        Id = id;
+        Title = title;
+        Path = path;
+        Properties = properties;
+    }
+}
---
+++ Screenbox.Core/Models/VideoInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.Storage.FileProperties;
+
+namespace Screenbox.Core.Models;
+public sealed record VideoInfo
+{
+    public string Title { get; set; } = string.Empty;
+    public string Subtitle { get; set; } = string.Empty;
+    public string Producers { get; set; } = string.Empty;
+    public string Writers { get; set; } = string.Empty;
+    public TimeSpan Duration { get; set; }
+    public uint Year { get; set; }
+    public uint Width { get; set; }
+    public uint Height { get; set; }
+    public uint Bitrate { get; set; }
+
+    /** VLC metadata **/
+    public string ShowName { get; set; } = string.Empty;
+    public string Season { get; set; } = string.Empty;
+    public string Episode { get; set; } = string.Empty;
+
+    public VideoInfo() { }
+
+    public VideoInfo(VideoProperties videoProperties)
+    {
+        Title = videoProperties.Title;
+        Subtitle = videoProperties.Subtitle;
+        Year = videoProperties.Year;
+        Producers = string.Join(", ", videoProperties.Producers);
+        Writers = string.Join(", ", videoProperties.Writers);
+        Duration = videoProperties.Duration;
+        Width = videoProperties.Width;
+        Height = videoProperties.Height;
+        Bitrate = videoProperties.Bitrate;
+    }
+}
