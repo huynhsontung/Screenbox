f671bfefe0ead5765437a16719b6e4a5f43be402
Tung Huynh <tung75605@gmail.com>
2024-04-12 00:50:45 -0700

    fix navigation from playlist flyout when the player is visible does not work as expected

--- Screenbox.Core/Services/INavigationService.cs
+++ Screenbox.Core/Services/INavigationService.cs
@@ -6,6 +6,7 @@ namespace Screenbox.Core.Services
 {
     public interface INavigationService
     {
+        event EventHandler? Navigated;
         void Navigate(Type vmType, object? parameter = null);
         void NavigateChild(Type parentVmType, Type targetVmType, object? parameter = null);
         void NavigateExisting(Type vmType, object? parameter = null);
