1575e9c56609674b275676735872b69c51ffc3e7
Tung Huynh <tung75605@gmail.com>
2024-07-01 17:09:54 -0700

    remove sort by button on artists page

--- Screenbox/Pages/ArtistsPage.xaml.cs
+++ Screenbox/Pages/ArtistsPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
 using CommunityToolkit.WinUI;
 using Screenbox.Core.ViewModels;
-using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
@@ -64,12 +63,6 @@ namespace Screenbox.Pages
         private void ArtistGridView_OnContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
         {
             // TODO: Load artist image from the internet
-        }
-
-        private string GetSortByText(string tag)
-        {
-            var item = SortByFlyout.Items?.FirstOrDefault(x => x.Tag as string == tag) ?? SortByFlyout.Items?.FirstOrDefault();
-            return (item as MenuFlyoutItem)?.Text ?? string.Empty;
         }
     }
 }
