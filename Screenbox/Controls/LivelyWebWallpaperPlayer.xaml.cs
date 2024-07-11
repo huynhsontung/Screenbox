01ce813da1e8f8dfe95a6405c4bb66541764bdcf
Tung Huynh <tung75605@gmail.com>
2024-07-11 01:14:16 -0700

    update lively wallpaper player control

--- Screenbox/Controls/LivelyWebWallpaperPlayer.xaml.cs
+++ Screenbox/Controls/LivelyWebWallpaperPlayer.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿#nullable enable
+
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.Web.WebView2.Core;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -6,6 +8,7 @@ using Screenbox.Core.Helpers;
 using Screenbox.Core.Models;
 using Screenbox.Core.ViewModels;
 using System;
+using System.ComponentModel;
 using System.IO;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
@@ -22,7 +25,7 @@ namespace Screenbox.Controls;
 // Source: https://github.com/rocksdanister/lively
 public sealed partial class LivelyWebWallpaperPlayer : UserControl
 {
-    public LivelyWallpaperModel Source
+    public LivelyWallpaperModel? Source
     {
         get { return (LivelyWallpaperModel)GetValue(SourceProperty); }
         set
@@ -35,24 +38,20 @@ public sealed partial class LivelyWebWallpaperPlayer : UserControl
     public static readonly DependencyProperty SourceProperty =
         DependencyProperty.Register("Source", typeof(LivelyWallpaperModel), typeof(LivelyWebWallpaperPlayer), new PropertyMetadata(null));
 
-    public MediaViewModel Media
+    public MediaViewModel? Media
     {
-        get { return (MediaViewModel)GetValue(MediaProperty); }
-        set
-        {
-            SetValue(MediaProperty, value);
-            _ = UpdateMusic();
-        }
+        get => (MediaViewModel?)GetValue(MediaProperty);
+        set => SetValue(MediaProperty, value);
     }
 
     public static readonly DependencyProperty MediaProperty =
-        DependencyProperty.Register("Media", typeof(MediaViewModel), typeof(LivelyWebWallpaperPlayer), new PropertyMetadata(null));
+        DependencyProperty.Register("Media", typeof(MediaViewModel), typeof(LivelyWebWallpaperPlayer), new PropertyMetadata(null, OnMediaChanged));
 
     public double[] Audio
     {
         get { return (double[])GetValue(AudioProperty); }
-        set 
-        { 
+        set
+        {
             SetValue(AudioProperty, value);
             _ = UpdateAudio();
         }
@@ -70,39 +69,40 @@ public sealed partial class LivelyWebWallpaperPlayer : UserControl
     public static readonly DependencyProperty IsLoadingProperty =
         DependencyProperty.Register("IsLoading", typeof(bool), typeof(LivelyWebWallpaperPlayer), new PropertyMetadata(false));
 
-    // Use this in case control VisibilityProperty is not helpful, ie parent control visibility changed.
-    // WebView rendering process stops if parent is Collapsed even without this, but we need this to queue wallpaper events.
-    // This property may not be required in the future, ref: https://github.com/microsoft/microsoft-ui-xaml/issues/674
-    public Visibility EffectiveVisibility
-    {
-        get { return (Visibility)GetValue(EffectiveVisibilityProperty); }
-        set 
-        { 
-            SetValue(EffectiveVisibilityProperty, value);
-            _ = UpdateVisibility(value);
-        }
-    }
-
-    public static readonly DependencyProperty EffectiveVisibilityProperty =
-        DependencyProperty.Register("EffectiveVisibility", typeof(Visibility), typeof(LivelyWebWallpaperPlayer), new PropertyMetadata(Visibility.Visible));
-
-    public event EventHandler<Exception> WallpaperError;
+    public event EventHandler<Exception>? WallpaperError;
 
     // We handle the WebView life-cycle by code.
-    private WebView2 webView;
-    private bool isWebViewInitialized;
-    private ulong currentNavigationId;
-    // See EffectiveVisibility
-    private Visibility currentVisibility;
+    private WebView2? _webView;
+    private bool _isWebViewInitialized;
+    private ulong _currentNavigationId;
+
+    private long _propertyChangedToken;
 
     public LivelyWebWallpaperPlayer()
     {
         this.InitializeComponent();
-        this.RegisterPropertyChangedCallback(VisibilityProperty, UserControl_VisibilityChanged);
+    }
+
+    private static async void OnMediaChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (LivelyWebWallpaperPlayer)d;
+        if (e.OldValue is MediaViewModel oldMedia) oldMedia.PropertyChanged -= control.MediaOnPropertyChanged;
+        if (e.NewValue is MediaViewModel newValue) newValue.PropertyChanged += control.MediaOnPropertyChanged;
+        await control.UpdateMusic();
+    }
+
+    private async void MediaOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(MediaViewModel.Name) or nameof(MediaViewModel.MainArtist))
+        {
+            await UpdateMusic();
+        }
     }
 
     private async void UserControl_Loaded(object sender, RoutedEventArgs e)
     {
+        _propertyChangedToken = RegisterPropertyChangedCallback(VisibilityProperty, OnVisibilityChanged);
+
         await InitializeWebView2();
         UpdatePage();
     }
@@ -110,38 +110,40 @@ public sealed partial class LivelyWebWallpaperPlayer : UserControl
     // x:Load xaml can be used to close and or restart WebView process.
     private void UserControl_Unloaded(object sender, RoutedEventArgs e)
     {
+        UnregisterPropertyChangedCallback(VisibilityProperty, _propertyChangedToken);
+        if (Media != null) Media.PropertyChanged -= MediaOnPropertyChanged;
         CloseWebView2();
     }
 
-    private async void UserControl_VisibilityChanged(DependencyObject sender, DependencyProperty dp)
+    private async void OnVisibilityChanged(DependencyObject sender, DependencyProperty dp)
     {
-        await UpdateVisibility(((UIElement)sender).Visibility);
+        await UpdateVisibility(Visibility);
     }
 
     private async Task InitializeWebView2()
     {
         try
         {
-            if (webView is not null)
+            if (_webView is not null)
                 CloseWebView2();
 
-            webView = new WebView2();
-            webView.NavigationStarting += WebView_NavigationStarting;
-            webView.NavigationCompleted += WebView_NavigationCompleted;
-            LayoutRoot.Children.Add(webView);
+            _webView = new WebView2();
+            _webView.NavigationStarting += WebView_NavigationStarting;
+            _webView.NavigationCompleted += WebView_NavigationCompleted;
+            LayoutRoot.Children.Add(_webView);
 
-            await webView.EnsureCoreWebView2Async();
+            await _webView.EnsureCoreWebView2Async();
 
             // Theme need to set css, ref: https://github.com/MicrosoftEdge/WebView2Feedback/issues/4426
-            webView.CoreWebView2.Profile.PreferredColorScheme = CoreWebView2PreferredColorScheme.Auto;
+            _webView.CoreWebView2.Profile.PreferredColorScheme = CoreWebView2PreferredColorScheme.Auto;
             // Don't allow contextmenu and devtools.
-            webView.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = false;
-            webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            _webView.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = false;
+            _webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
             // Mute audio output.
-            webView.CoreWebView2.IsMuted = true;
-            // webView.CoreWebView2.OpenDevToolsWindow();
+            _webView.CoreWebView2.IsMuted = true;
+            _webView.CoreWebView2.OpenDevToolsWindow();
 
-            isWebViewInitialized = true;
+            _isWebViewInitialized = true;
         }
         catch (Exception ex)
         {
@@ -151,21 +153,21 @@ public sealed partial class LivelyWebWallpaperPlayer : UserControl
 
     private void CloseWebView2()
     {
-        webView.Close();
-        LayoutRoot.Children.Remove(webView);
-        isWebViewInitialized = false;
-        webView = null;
+        _webView?.Close();
+        LayoutRoot.Children.Remove(_webView);
+        _isWebViewInitialized = false;
+        _webView = null;
     }
 
-    private void WebView_NavigationStarting(WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs args)
+    private void WebView_NavigationStarting(WebView2 sender, CoreWebView2NavigationStartingEventArgs args)
     {
-        currentNavigationId = args.NavigationId;
+        _currentNavigationId = args.NavigationId;
         IsLoading = true;
     }
 
     private async void WebView_NavigationCompleted(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
     {
-        if (args.NavigationId != currentNavigationId)
+        if (args.NavigationId != _currentNavigationId)
             return;
 
         await UpdateLivelyProperties();
@@ -175,17 +177,16 @@ public sealed partial class LivelyWebWallpaperPlayer : UserControl
 
     private void UpdatePage()
     {
-        if (!isWebViewInitialized || Source is null)
+        if (!_isWebViewInitialized || Source is null)
             return;
 
         var htmlPath = Path.Combine(Source.Path, Source.Model.FileName);
-        webView.NavigateToLocalPath(htmlPath);
+        _webView.NavigateToLocalPath(htmlPath);
     }
 
     private async Task UpdateVisibility(Visibility visibility)
     {
-        currentVisibility = visibility;
-        if (!isWebViewInitialized)
+        if (!_isWebViewInitialized || IsLoading)
             return;
 
         switch (visibility)
@@ -205,13 +206,14 @@ public sealed partial class LivelyWebWallpaperPlayer : UserControl
                 }
                 break;
         }
-        await UpdatePauseState();
+
+        // await UpdatePauseState();
     }
 
     // Ref: https://github.com/rocksdanister/lively/wiki/Web-Guide-IV-:-Interaction#controls
     private async Task UpdateLivelyProperties()
     {
-        if (Source is null || webView is null)
+        if (Source is null || _webView is null)
             return;
 
         var functionName = "livelyPropertyListener";
@@ -226,21 +228,21 @@ public sealed partial class LivelyWebWallpaperPlayer : UserControl
             switch (item.Value["type"].ToString())
             {
                 case "slider":
-                    await webView.ExecuteScriptFunctionAsync(functionName, item.Key, (double)item.Value["value"]);
+                    await _webView.ExecuteScriptFunctionAsync(functionName, item.Key, (double)item.Value["value"]);
                     break;
                 case "dropdown":
-                    await webView.ExecuteScriptFunctionAsync(functionName, item.Key, (int)item.Value["value"]);
+                    await _webView.ExecuteScriptFunctionAsync(functionName, item.Key, (int)item.Value["value"]);
                     break;
                 case "checkbox":
-                    await webView.ExecuteScriptFunctionAsync(functionName, item.Key, (bool)item.Value["value"]);
+                    await _webView.ExecuteScriptFunctionAsync(functionName, item.Key, (bool)item.Value["value"]);
                     break;
                 case "color":
-                    await webView.ExecuteScriptFunctionAsync(functionName, item.Key, (string)item.Value["value"]);
+                    await _webView.ExecuteScriptFunctionAsync(functionName, item.Key, (string)item.Value["value"]);
                     break;
                 case "folderDropdown":
                     var relativePath = Path.Combine(item.Value["folder"].ToString(), item.Value["value"].ToString());
                     var filePath = Path.Combine(Source.Path, relativePath);
-                    await webView.ExecuteScriptFunctionAsync(functionName, item.Key, File.Exists(filePath) ? relativePath : null);
+                    await _webView.ExecuteScriptFunctionAsync(functionName, item.Key, File.Exists(filePath) ? relativePath : null);
                     break;
                 case "button":
                 case "label":
@@ -254,57 +256,57 @@ public sealed partial class LivelyWebWallpaperPlayer : UserControl
     private async Task UpdateMusic()
     {
         // WebView rendering process pauses when visibility is hidden.
-        if (Source is null || webView is null || !Source.IsMusic || currentVisibility == Visibility.Collapsed)
+        if (Source == null || (_webView?.CoreWebView2.IsSuspended ?? true) || !Source.IsMusic || Visibility == Visibility.Collapsed)
             return;
 
-        var base64 = await StorageItemToBase64(this.Media?.ThumbnailSource);
-        var obj = Media is null ? null : new LivelyMusicModel()
+        LivelyMusicModel? model = null;
+        if (Media != null)
         {
-            Title = Media.Name,
-            Artist = Media.Album?.ArtistName,
-            Thumbnail = base64,
-            // Optional: Complete the mapping.
-        };
-        await webView.ExecuteScriptFunctionAsync("livelyCurrentTrack", JsonConvert.SerializeObject(obj, Formatting.Indented));
+            var base64 = await StorageItemToBase64(Media.ThumbnailSource);
+            model = new LivelyMusicModel
+            {
+                Title = Media.Name,
+                Artist = Media.MainArtist?.Name,
+                Thumbnail = base64,
+                // Optional: Complete the mapping.
+            };
+        }
+
+        await _webView.ExecuteScriptFunctionAsync("livelyCurrentTrack", JsonConvert.SerializeObject(model));
     }
 
     // Ref: https://github.com/rocksdanister/lively/wiki/Web-Guide-V-:-System-Data#--audio
     private async Task UpdateAudio()
     {
         // WebView rendering process pauses when visibility is hidden.
-        if (Source is null || webView is null || !Source.IsAudio || currentVisibility == Visibility.Collapsed)
+        if (Source == null || (_webView?.CoreWebView2.IsSuspended ?? true) || !Source.IsMusic)
             return;
 
-        await webView.ExecuteScriptFunctionAsync("livelyAudioListener", Audio);
+        await _webView.ExecuteScriptFunctionAsync("livelyAudioListener", Audio);
     }
 
     // Ref: https://github.com/rocksdanister/lively/wiki/Web-Guide-V-:-System-Data#--pause-event
     private async Task UpdatePauseState()
     {
-        if (Source is null || webView is null || !Source.IsPauseNotify)
+        if (Source is null || _webView is null || !Source.IsPauseNotify)
             return;
 
-        var obj = new LivelyPlaybackStateModel()
-        {
-            IsPaused = currentVisibility == Visibility.Collapsed
-        };
-        await webView.ExecuteScriptFunctionAsync("livelyWallpaperPlaybackChanged", obj);
+        // var obj = new LivelyPlaybackStateModel()
+        // {
+        //     IsPaused = _currentVisibility == Visibility.Collapsed
+        // };
+        // await _webView.ExecuteScriptFunctionAsync("livelyWallpaperPlaybackChanged", obj);
     }
 
-    private async Task<string> StorageItemToBase64(StorageItemThumbnail item)
+    private static async Task<string> StorageItemToBase64(StorageItemThumbnail? item)
     {
         if (item == null)
-            return null;
+            return string.Empty;
 
-        using var memoryStream = new MemoryStream();
-        using var inputStream = item.CloneStream();
+        using var stream = item.CloneStream();
 
-        var buffer = new byte[inputStream.Size];
-        await inputStream.ReadAsync(buffer.AsBuffer(), (uint)inputStream.Size, InputStreamOptions.None);
-
-        await memoryStream.WriteAsync(buffer, 0, buffer.Length);
-        memoryStream.Seek(0, SeekOrigin.Begin);
-
-        return Convert.ToBase64String(memoryStream.ToArray());
+        var buffer = new byte[stream.Size];
+        await stream.ReadAsync(buffer.AsBuffer(), (uint)stream.Size, InputStreamOptions.None);
+        return Convert.ToBase64String(buffer);
     }
 }
