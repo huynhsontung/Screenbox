61e423772895a41cc179b521942a8c0f9b7797a8
United600 <United600@users.noreply.github.com>
2024-08-06 01:21:24 +0100

    add behavior comments

--- Screenbox/Controls/Interactions/NavigationViewAccessKeysBehavior.cs
+++ Screenbox/Controls/Interactions/NavigationViewAccessKeysBehavior.cs
@@ -8,72 +8,120 @@ using NavigationView = Microsoft.UI.Xaml.Controls.NavigationView;
 namespace Screenbox.Controls.Interactions;
 internal class NavigationViewAccessKeysBehavior : BehaviorBase<NavigationView>
 {
+    ///// <summary>
+    ///// The dependency property for <see cref="BackButtonAccessKey"/>.
+    ///// </summary>
     //public static readonly DependencyProperty BackButtonAccessKeyProperty = DependencyProperty.Register(
     //    nameof(BackButtonAccessKey), typeof(string), typeof(NavigationViewAccessKeysBehavior), new PropertyMetadata(string.Empty));
 
+    ///// <summary>
+    ///// Get and set the <see cref="Microsoft.UI.Xaml.Controls.NavigationView"/> back button access key.
+    ///// </summary>
     //public string BackButtonAccessKey
     //{
     //    get => (string)GetValue(BackButtonAccessKeyProperty);
     //    set => SetValue(BackButtonAccessKeyProperty, value);
     //}
 
+    ///// <summary>
+    ///// The dependency property for <see cref="BackButtonKeyTipPlacementMode"/>.
+    ///// </summary>
     //public static readonly DependencyProperty BackButtonKeyTipPlacementModeProperty = DependencyProperty.Register(
     //    nameof(BackButtonKeyTipPlacementMode), typeof(KeyTipPlacementMode), typeof(NavigationViewAccessKeysBehavior), new PropertyMetadata(default(KeyTipPlacementMode)));
 
+    ///// <summary>
+    ///// Get and set the <see cref="Microsoft.UI.Xaml.Controls.NavigationView"/> back button access key placement.
+    ///// </summary>
     //public KeyTipPlacementMode BackButtonKeyTipPlacementMode
     //{
     //    get => (KeyTipPlacementMode)GetValue(BackButtonKeyTipPlacementModeProperty);
     //    set => SetValue(BackButtonKeyTipPlacementModeProperty, value);
     //}
 
+    ///// <summary>
+    ///// The dependency property for <see cref="CloseButtonAccessKey"/>.
+    ///// </summary>
     //public static readonly DependencyProperty CloseButtonAccessKeyProperty = DependencyProperty.Register(
     //    nameof(CloseButtonAccessKey), typeof(string), typeof(NavigationViewAccessKeysBehavior), new PropertyMetadata(string.Empty));
 
+    ///// <summary>
+    ///// Get and set the <see cref="Microsoft.UI.Xaml.Controls.NavigationView"/> close button access key.
+    ///// </summary>
     //public string CloseButtonAccessKey
     //{
     //    get => (string)GetValue(CloseButtonAccessKeyProperty);
     //    set => SetValue(CloseButtonAccessKeyProperty, value);
     //}
 
+    ///// <summary>
+    ///// The dependency property for <see cref="CloseButtonKeyTipPlacementMode"/>.
+    ///// </summary>
     //public static readonly DependencyProperty CloseButtonKeyTipPlacementModeProperty = DependencyProperty.Register(
     //    nameof(CloseButtonKeyTipPlacementMode), typeof(KeyTipPlacementMode), typeof(NavigationViewAccessKeysBehavior), new PropertyMetadata(default(KeyTipPlacementMode)));
 
+    ///// <summary>
+    ///// Get and set the <see cref="Microsoft.UI.Xaml.Controls.NavigationView"/> close button access key placement.
+    ///// </summary>
     //public KeyTipPlacementMode CloseButtonKeyTipPlacementMode
     //{
     //    get => (KeyTipPlacementMode)GetValue(CloseButtonKeyTipPlacementModeProperty);
     //    set => SetValue(CloseButtonKeyTipPlacementModeProperty, value);
     //}
 
+    ///// <summary>
+    ///// The dependency property for <see cref="TogglePaneButtonAccessKey"/>.
+    ///// </summary>
     //public static readonly DependencyProperty TogglePaneButtonAccessKeyProperty = DependencyProperty.Register(
     //    nameof(TogglePaneButtonAccessKey), typeof(string), typeof(NavigationViewAccessKeysBehavior), new PropertyMetadata(string.Empty));
 
+    ///// <summary>
+    ///// Get and set the <see cref="Microsoft.UI.Xaml.Controls.NavigationView"/> pane button access key.
+    ///// </summary>
     //public string TogglePaneButtonAccessKey
     //{
     //    get => (string)GetValue(TogglePaneButtonAccessKeyProperty);
     //    set => SetValue(TogglePaneButtonAccessKeyProperty, value);
     //}
 
+    ///// <summary>
+    ///// The dependency property for <see cref="TogglePaneButtonKeyTipPlacementMode"/>.
+    ///// </summary>
     //public static readonly DependencyProperty TogglePaneButtonKeyTipPlacementModeProperty = DependencyProperty.Register(
     //    nameof(TogglePaneButtonKeyTipPlacementMode), typeof(KeyTipPlacementMode), typeof(NavigationViewAccessKeysBehavior), new PropertyMetadata(null));
 
+    ///// <summary>
+    ///// Get and set the <see cref="Microsoft.UI.Xaml.Controls.NavigationView"/> pane button access key placement.
+    ///// </summary>
     //public KeyTipPlacementMode TogglePaneButtonKeyTipPlacementMode
     //{
     //    get => (KeyTipPlacementMode)GetValue(TogglePaneButtonKeyTipPlacementModeProperty);
     //    set => SetValue(TogglePaneButtonKeyTipPlacementModeProperty, value);
     //}
 
+    ///// <summary>
+    ///// The dependency property for <see cref="PaneAutoSuggestButtonAccessKey"/>.
+    ///// </summary>
     //public static readonly DependencyProperty PaneAutoSuggestButtonAccessKeyProperty = DependencyProperty.Register(
     //    nameof(PaneAutoSuggestButtonAccessKey), typeof(string), typeof(NavigationViewAccessKeysBehavior), new PropertyMetadata(string.Empty));
 
+    ///// <summary>
+    ///// Get and set the <see cref="Microsoft.UI.Xaml.Controls.NavigationView"/> auto suggest button access key.
+    ///// </summary>
     //public string PaneAutoSuggestButtonAccessKey
     //{
     //    get => (string)GetValue(PaneAutoSuggestButtonAccessKeyProperty);
     //    set => SetValue(PaneAutoSuggestButtonAccessKeyProperty, value);
     //}
 
+    ///// <summary>
+    ///// The dependency property for <see cref="PaneAutoSuggestButtonKeyTipPlacementMode"/>.
+    ///// </summary>
     //public static readonly DependencyProperty PaneAutoSuggestButtonKeyTipPlacementModeProperty = DependencyProperty.Register(
     //    nameof(PaneAutoSuggestButtonKeyTipPlacementMode), typeof(KeyTipPlacementMode), typeof(NavigationViewAccessKeysBehavior), new PropertyMetadata(default(KeyTipPlacementMode)));
 
+    ///// <summary>
+    ///// Get and set the <see cref="Microsoft.UI.Xaml.Controls.NavigationView"/> auto suggest button access key placement.
+    ///// </summary>
     //public KeyTipPlacementMode PaneAutoSuggestButtonKeyTipPlacementMode
     //{
     //    get => (KeyTipPlacementMode)GetValue(PaneAutoSuggestButtonKeyTipPlacementModeProperty);
@@ -84,7 +132,7 @@ internal class NavigationViewAccessKeysBehavior : BehaviorBase<NavigationView>
     {
         base.OnAssociatedObjectLoaded();
 
-        if (AssociatedObject.FindDescendant<Button>(btn => btn.Name == "NavigationViewBackButton") is { } navigationViewBackButton)
+        if (AssociatedObject.FindDescendant<Button>(bb => bb.Name == "NavigationViewBackButton") is { } navigationViewBackButton)
         {
             navigationViewBackButton.AccessKey = Strings.KeyboardResources.NavBackButtonKey;
             navigationViewBackButton.KeyTipPlacementMode = KeyTipPlacementMode.Right;
@@ -93,7 +141,7 @@ internal class NavigationViewAccessKeysBehavior : BehaviorBase<NavigationView>
             
         }
 
-        if (AssociatedObject.FindDescendant<Button>(btn => btn.Name == "NavigationViewCloseButton") is { } navigationViewCloseButton)
+        if (AssociatedObject.FindDescendant<Button>(cb => cb.Name == "NavigationViewCloseButton") is { } navigationViewCloseButton)
         {
             navigationViewCloseButton.AccessKey = Strings.KeyboardResources.NavCloseButtonKey;
             navigationViewCloseButton.KeyTipPlacementMode = KeyTipPlacementMode.Bottom;
@@ -101,7 +149,7 @@ internal class NavigationViewAccessKeysBehavior : BehaviorBase<NavigationView>
             //navigationViewCloseButton.KeyTipPlacementMode = CloseButtonKeyTipPlacementMode;
         }
 
-        if (AssociatedObject.FindDescendant<Button>(btn => btn.Name == "TogglePaneButton") is { } togglePaneButton)
+        if (AssociatedObject.FindDescendant<Button>(pb => pb.Name == "TogglePaneButton") is { } togglePaneButton)
         {
             togglePaneButton.AccessKey = Strings.KeyboardResources.NavToggleMenuPaneButtonKey;
             togglePaneButton.KeyTipPlacementMode = KeyTipPlacementMode.Right;
@@ -110,13 +158,16 @@ internal class NavigationViewAccessKeysBehavior : BehaviorBase<NavigationView>
             
         }
 
-        if (AssociatedObject.FindDescendant<Button>(btn => btn.Name == "PaneAutoSuggestButton") is { } paneAutoSuggestButton)
+        if (AssociatedObject.FindDescendant<Button>(sb => sb.Name == "PaneAutoSuggestButton") is { } paneAutoSuggestButton)
         {
             paneAutoSuggestButton.AccessKey = Strings.KeyboardResources.NavAutoSuggestButtonKey;
             paneAutoSuggestButton.KeyTipPlacementMode = KeyTipPlacementMode.Right;
-            paneAutoSuggestButton.IsTextScaleFactorEnabled = false;
             //paneAutoSuggestButton.AccessKey = PaneAutoSuggestButtonAccessKey;
             //paneAutoSuggestButton.KeyTipPlacementMode = PaneAutoSuggestButtonKeyTipPlacementMode;
+
+            // Disable button glyph text scaling
+            // <see href="https://learn.microsoft.com/en-us/windows/apps/design/input/text-scaling#dont-scale-font-based-icons-or-symbols"/>
+            paneAutoSuggestButton.IsTextScaleFactorEnabled = false;
         }
     }
 }
