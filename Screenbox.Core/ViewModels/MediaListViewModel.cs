84acbf83ca82fe054912e5ad8fb23f498da8fc2f
Tung Huynh <tung75605@gmail.com>
2025-01-09 22:34:30 -0800

    fix: media not playing when launch from file try to play the media when the player is not initialized

--- Screenbox.Core/ViewModels/MediaListViewModel.cs
+++ Screenbox.Core/ViewModels/MediaListViewModel.cs
@@ -133,8 +133,15 @@ namespace Screenbox.Core.ViewModels
             {
                 async void SetPlayQueue()
                 {
-                    ClearPlaylist();
-                    await EnqueueAndPlay(_delayPlay);
+                    if (_delayPlay is MediaViewModel media && Items.Contains(media))
+                    {
+                        PlaySingle(media);
+                    }
+                    else
+                    {
+                        ClearPlaylist();
+                        await EnqueueAndPlay(_delayPlay);
+                    }
                 }
 
                 _dispatcherQueue.TryEnqueue(SetPlayQueue);
@@ -187,7 +194,15 @@ namespace Screenbox.Core.ViewModels
                 // If there is only 1 file, play it immediately
                 // Avoid waiting to get all the neighboring files then play, which may cause delay
                 ClearPlaylist();
-                await EnqueueAndPlay(file);
+                if (_mediaPlayer == null)
+                {
+                    _delayPlay = media;
+                }
+                else
+                {
+                    await EnqueueAndPlay(file);
+                }
+
                 // If there are more than one item in the queue, file is already a playlist, no need to check for neighboring files
                 if (Items.Count > 1) return;
 
