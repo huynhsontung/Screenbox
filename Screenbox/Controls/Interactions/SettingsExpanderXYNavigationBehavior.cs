f19f2bfb464e4031d22a0a6a67ab82a53fcf4d4d
United600 <United600@users.noreply.github.com>
2025-01-27 09:21:54 +0000

    chore: minor settings page improvements (#537)
    * remove settingscard merged dictionary
    
    * add settings enqueue files RTL icon
    
    * streamline settings expander xy behavior
    
    * add narrator help text to content

--- Screenbox/Controls/Interactions/SettingsExpanderXYNavigationBehavior.cs
+++ Screenbox/Controls/Interactions/SettingsExpanderXYNavigationBehavior.cs
@@ -1,22 +1,46 @@
 ﻿using CommunityToolkit.WinUI;
-using CommunityToolkit.WinUI.Behaviors;
 using CommunityToolkit.WinUI.Controls;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 
 namespace Screenbox.Controls.Interactions;
-internal class SettingsExpanderXYNavigationBehavior : BehaviorBase<SettingsExpander>
-{
-    public Control? XYFocusRight { get; set; }
 
-    protected override void OnAssociatedObjectLoaded()
+/// <summary>
+/// A behavior that makes the <see cref="SettingsExpander"/> content accessible to XY focus navigation.
+/// </summary>
+internal class SettingsExpanderXYNavigationBehavior : Behavior<SettingsExpander>
+{
+    protected override void OnAttached()
     {
-        base.OnAssociatedObjectLoaded();
-        if (XYFocusRight == null) return;
-        if (AssociatedObject.FindDescendant<ToggleButton>() is { } button)
+        base.OnAttached();
+        AssociatedObject.Loaded += AssociatedObjectOnLoaded;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        AssociatedObject.Loaded -= AssociatedObjectOnLoaded;
+    }
+
+    private void AssociatedObjectOnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is SettingsExpander settingsExpander)
         {
-            button.XYFocusRight = XYFocusRight;
-            XYFocusRight.XYFocusRight = button;
+            ToggleButton toggleButton = settingsExpander.FindDescendant<ToggleButton>(e => e.Name == "ExpanderHeader");
+            if (toggleButton != null)
+            {
+                ContentPresenter contentPresenter = toggleButton.FindDescendant<ContentPresenter>(c => c.Name == "PART_ContentPresenter");
+                if (contentPresenter != null)
+                {
+                    toggleButton.XYFocusRight = contentPresenter;
+
+                    // Ensure that when using the D-pad or left stick to navigate left or right, focus will move to the associated settings expander
+                    contentPresenter.XYFocusLeftNavigationStrategy = Windows.UI.Xaml.Input.XYFocusNavigationStrategy.NavigationDirectionDistance;
+                    contentPresenter.XYFocusRightNavigationStrategy = Windows.UI.Xaml.Input.XYFocusNavigationStrategy.NavigationDirectionDistance;
+                }
+            }
         }
     }
 }
