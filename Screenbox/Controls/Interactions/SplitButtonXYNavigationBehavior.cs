776812d792b0b6f1a1bc7133559094626df18fc1
United600 <United600@users.noreply.github.com>
2025-01-28 22:23:38 +0000

    improve split button xy navigation behavior

--- Screenbox/Controls/Interactions/SplitButtonXYNavigationBehavior.cs
+++ Screenbox/Controls/Interactions/SplitButtonXYNavigationBehavior.cs
@@ -1,96 +1,46 @@
 ﻿using CommunityToolkit.WinUI;
-using CommunityToolkit.WinUI.Behaviors;
+using Microsoft.Xaml.Interactivity;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Automation;
-using Windows.UI.Xaml.Automation.Peers;
 using Windows.UI.Xaml.Controls;
-//using Windows.UI.Xaml.Input;
+
+using SplitButton = Microsoft.UI.Xaml.Controls.SplitButton;
 
 namespace Screenbox.Controls.Interactions;
 
 /// <summary>
-/// A behavior that sets a <see cref="ToolTip"/> to each part of the <see cref="Microsoft.UI.Xaml.Controls.SplitButton"/>,
-/// enables navigation to the secondary button and ensures the drop down access key display is not dismissed when invoked.
+/// A behavior that makes the <see cref="SplitButton"/> secondary button accessible to XY focus navigation.
 /// </summary>
-internal class SplitButtonXYNavigationBehavior : BehaviorBase<Microsoft.UI.Xaml.Controls.SplitButton>
+internal class SplitButtonXYNavigationBehavior : Behavior<SplitButton>
 {
-    /// <summary>
-    /// Identifies the <see cref="PrimaryButtonTooltip"/> dependency property.
-    /// </summary>
-    public static readonly DependencyProperty PrimaryButtonTooltipProperty = DependencyProperty.Register(
-        nameof(PrimaryButtonTooltip), typeof(string), typeof(SplitButtonXYNavigationBehavior), new PropertyMetadata(null));
-
-    /// <summary>
-    /// Gets or sets the tooltip associated with the primary button.
-    /// </summary>
-    public string PrimaryButtonTooltip
+    protected override void OnAttached()
     {
-        get => (string)GetValue(PrimaryButtonTooltipProperty);
-        set => SetValue(PrimaryButtonTooltipProperty, value);
+        base.OnAttached();
+        AssociatedObject.Loaded += AssociatedObjectOnLoaded;
     }
 
-    /// <summary>
-    /// Identifies the <see cref="SecondaryButtonTooltip"/> dependency property.
-    /// </summary>
-    public static readonly DependencyProperty SecondaryButtonTooltipProperty = DependencyProperty.Register(
-        nameof(SecondaryButtonTooltip), typeof(string), typeof(SplitButtonXYNavigationBehavior), new PropertyMetadata(null));
-
-    /// <summary>
-    /// Gets or sets the tooltip associated with the secondary button.
-    /// </summary>
-    public string SecondaryButtonTooltip
+    protected override void OnDetaching()
     {
-        get => (string)GetValue(SecondaryButtonTooltipProperty);
-        set => SetValue(SecondaryButtonTooltipProperty, value);
+        base.OnDetaching();
+        AssociatedObject.Loaded -= AssociatedObjectOnLoaded;
     }
 
-    /// <summary>
-    /// Identifies the <see cref="SecondaryButtonAccessKey"/> dependency property.
-    /// </summary>
-    public static readonly DependencyProperty SecondaryButtonAccessKeyProperty = DependencyProperty.Register(
-        nameof(SecondaryButtonAccessKey), typeof(string), typeof(SplitButtonXYNavigationBehavior), new PropertyMetadata(null));
-
-    /// <summary>
-    /// Gets or sets the access key of the secondary button.
-    /// </summary>
-    public string SecondaryButtonAccessKey
+    private void AssociatedObjectOnLoaded(object sender, RoutedEventArgs e)
     {
-        get => (string)GetValue(SecondaryButtonAccessKeyProperty);
-        set => SetValue(SecondaryButtonAccessKeyProperty, value);
-    }
-
-    protected override void OnAssociatedObjectLoaded()
-    {
-        base.OnAssociatedObjectLoaded();
-
-        //if (AssociatedObject is Microsoft.UI.Xaml.Controls.SplitButton splitButton)
-        //{
-        //    splitButton.XYFocusRightNavigationStrategy = XYFocusNavigationStrategy.NavigationDirectionDistance;
-        //}
-
-        if (AssociatedObject.FindDescendant<Button>(pb => pb.Name == "PrimaryButton") is { } primaryButton)
+        if (sender is SplitButton splitButton)
         {
-            if (PrimaryButtonTooltip != null)
-            {
-                ToolTipService.SetToolTip(primaryButton, PrimaryButtonTooltip);
-            }
-        }
+            // Ensure that when navigating to the right with D-pad/left stick, focus will move to the secondary button
+            splitButton.XYFocusRightNavigationStrategy = Windows.UI.Xaml.Input.XYFocusNavigationStrategy.NavigationDirectionDistance;
 
-        if (AssociatedObject.FindDescendant<Button>(sb => sb.Name == "SecondaryButton") is { } secondaryButton)
-        {
-            if (SecondaryButtonAccessKey != null)
+            var secondaryButton = splitButton.FindDescendant<Button>(sb => sb.Name == "SecondaryButton");
+            if (secondaryButton != null)
             {
-                secondaryButton.AccessKey = SecondaryButtonAccessKey;
-                secondaryButton.ExitDisplayModeOnAccessKeyInvoked = false; // Don't dismiss the access key display upon invocation
-            }
+                secondaryButton.IsTabStop = true;
+                secondaryButton.XYFocusLeft = splitButton;
 
-            secondaryButton.IsTabStop = true; // Include the button in gamepad/tab navigation
-
-            if (SecondaryButtonTooltip != null)
-            {
-                ToolTipService.SetToolTip(secondaryButton, SecondaryButtonTooltip);
-                AutomationProperties.SetAccessibilityView(secondaryButton, AccessibilityView.Content); // Expose the button to the UI Automation tree
-                AutomationProperties.SetName(secondaryButton, SecondaryButtonTooltip); // Override the default automation name ("More Options") with the ToolTip content
+                // Adjust the button appearance to ensure its focus visual is consistent with the associated split button
+                CornerRadius cornerRadius = splitButton.CornerRadius;
+                secondaryButton.CornerRadius = new CornerRadius(0, cornerRadius.TopRight, cornerRadius.BottomRight, 0);
+                secondaryButton.FocusVisualMargin = splitButton.FocusVisualMargin;
             }
         }
     }
