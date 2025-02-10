84976ae1379ee555d8f93f583047c730134e3a8e
Tung Huynh <tung75605@gmail.com>
2025-02-10 01:13:01 -0800

    add convenient ThemeOption -> ElementTheme converter

---
+++ Screenbox.Core/Helpers/EnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Screenbox.Core.Enums;
+using System;
+using Windows.UI.Xaml;
+
+namespace Screenbox.Core.Helpers;
+public static class EnumExtensions
+{
+    public static ElementTheme ToElementTheme(this ThemeOption themeOption)
+    {
+        return themeOption switch
+        {
+            ThemeOption.Auto => ElementTheme.Default,
+            ThemeOption.Light => ElementTheme.Light,
+            ThemeOption.Dark => ElementTheme.Dark,
+            _ => throw new ArgumentOutOfRangeException(nameof(themeOption), themeOption, null),
+        };
+    }
+}
