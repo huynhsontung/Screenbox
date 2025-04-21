be93e6f3c3560bf3b3eb7b80370634dbb4d89e7c
United600 <United600@users.noreply.github.com>
2025-04-21 22:48:50 +0100

    add video track selection
    move separator to list views footer

--- Screenbox.Core/Playback/VideoTrack.cs
+++ Screenbox.Core/Playback/VideoTrack.cs
@@ -1,15 +1,25 @@
 ﻿#nullable enable
 
-namespace Screenbox.Core.Playback
-{
-    public sealed class VideoTrack : MediaTrack
-    {
-        public VideoTrack(LibVLCSharp.Shared.MediaTrack videoTrack) : base(videoTrack)
-        {
-        }
+using CommunityToolkit.Diagnostics;
+using LibVLCSharp.Shared;
 
-        public VideoTrack(Windows.Media.Core.VideoTrack videoTrack) : base(videoTrack)
-        {
-        }
+namespace Screenbox.Core.Playback;
+
+public sealed class VideoTrack : MediaTrack
+{
+    internal int VlcTrackId { get; }
+
+    public string Name { get; }
+
+    public VideoTrack(LibVLCSharp.Shared.MediaTrack videoTrack) : base(videoTrack)
+    {
+        Guard.IsTrue(videoTrack.TrackType == TrackType.Video, nameof(videoTrack.TrackType));
+        VlcTrackId = videoTrack.Id;
+        Name = videoTrack.Description ?? videoTrack.Language ?? videoTrack.Id.ToString();
+    }
+
+    public VideoTrack(Windows.Media.Core.VideoTrack videoTrack) : base(videoTrack)
+    {
+        Name = videoTrack.Name;
     }
 }
