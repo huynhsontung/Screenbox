d49d7669d9aa89eb69aa72c574d59411ee425473
United600 <United600@users.noreply.github.com>
2025-09-18 15:44:49 +0100

    minor grammar fixes and improve convert method nullability support

--- Screenbox/Converters/ResourceNameToResourceStringConverter.cs
+++ Screenbox/Converters/ResourceNameToResourceStringConverter.cs
@@ -12,7 +12,7 @@ using Windows.UI.Xaml.Data;
 namespace Screenbox.Converters;
 
 /// <summary>
-/// Value converter that look up for the source string in the App Resources strings and returns its value, if found.
+/// Value converter that looks up for the source string in the App Resources strings and returns its value, if found.
 /// </summary>
 public sealed class ResourceNameToResourceStringConverter : IValueConverter
 {
@@ -20,14 +20,14 @@ public sealed class ResourceNameToResourceStringConverter : IValueConverter
 
     /// <summary>
     /// Take the source string as a resource name that will be looked up in the App Resources.
-    /// If the resource exists, the value is returned; otherwise.
+    /// If the resource exists, the value is returned; otherwise, an empty string is returned.
     /// </summary>
     /// <param name="value">The source string being passed to the target.</param>
     /// <param name="targetType">The type of the target property, as a type reference.</param>
     /// <param name="parameter">Optional parameter. Not used.</param>
     /// <param name="language">The language of the conversion.</param>
     /// <returns>The string corresponding to the resource name.</returns>
-    public object? Convert(object value, Type targetType, object parameter, string language)
+    public object? Convert(object? value, Type targetType, object parameter, string language)
     {
         string? stringValue = value?.ToString();
         if (stringValue is null)
