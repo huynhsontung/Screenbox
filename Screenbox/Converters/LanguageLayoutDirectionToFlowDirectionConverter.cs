97cc0f95116cee2fe74f729ca49d94ec0f3d95c1
United600 <United600@users.noreply.github.com>
2025-09-29 06:17:40 +0100

    refactor LanguageLayoutDirection conversion logic (#679)

--- Screenbox/Converters/LanguageLayoutDirectionToFlowDirectionConverter.cs
+++ Screenbox/Converters/LanguageLayoutDirectionToFlowDirectionConverter.cs
@@ -4,13 +4,27 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
 namespace Screenbox.Converters;
-internal class LanguageLayoutDirectionToFlowDirectionConverter : IValueConverter
+
+public sealed class LanguageLayoutDirectionToFlowDirectionConverter : IValueConverter
 {
+    /// <summary>
+    /// Converts a <see cref="LanguageLayoutDirection"/> value to its corresponding <see cref="FlowDirection"/>.
+    /// </summary>
+    /// <param name="value">The <see cref="LanguageLayoutDirection"/> to convert.</param>
+    /// <returns><see cref="FlowDirection.RightToLeft"/> if <paramref name="value"/> is <see cref="LanguageLayoutDirection.Rtl"/> or
+    /// <see cref="LanguageLayoutDirection.TtbRtl"/>; otherwise, returns <see cref="FlowDirection.LeftToRight"/>.</returns>
+    public static FlowDirection ToFlowDirection(LanguageLayoutDirection value)
+    {
+        return value is LanguageLayoutDirection.Rtl or LanguageLayoutDirection.TtbRtl
+            ? FlowDirection.RightToLeft
+            : FlowDirection.LeftToRight;
+    }
+
     public object Convert(object value, Type targetType, object parameter, string language)
     {
-        if (value is (LanguageLayoutDirection.Rtl or LanguageLayoutDirection.TtbRtl))
+        if (value is LanguageLayoutDirection direction)
         {
-            return FlowDirection.RightToLeft;
+            return ToFlowDirection(direction);
         }
 
         return FlowDirection.LeftToRight;
