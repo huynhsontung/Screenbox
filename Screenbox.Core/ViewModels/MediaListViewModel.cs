80ff1ae864a11e3de4bc54b331bc41a84c4e7160
Tung Huynh <31434093+huynhsontung@users.noreply.github.com>
2025-11-09 12:30:48 -0800

    change order of operation when item change in MediaListViewModel
    All synchronous methods should executre before async updates. Use Task.WhenAll() to parallelize updates

--- Screenbox.Core/ViewModels/MediaListViewModel.cs
+++ Screenbox.Core/ViewModels/MediaListViewModel.cs
@@ -244,28 +244,14 @@ public sealed partial class MediaListViewModel : ObservableRecipient,
             }
             : null);
 
-        // Add to recent files
-        switch (value?.Source)
-        {
-            case StorageFile file:
-                _filesService.AddToRecent(file);
-                break;
-            case Uri { IsFile: true, IsLoopback: true, IsAbsoluteUri: true } uri:
-                try
-                {
-                    var file = await StorageFile.GetFileFromPathAsync(uri.OriginalString);
-                    _filesService.AddToRecent(file);
-                }
-                catch
-                {
-                    // ignored
-                }
-                break;
-        }
-
         Messenger.Send(new PlaylistCurrentItemChangedMessage(value));
-        await _transportControlsService.UpdateTransportControlsDisplayAsync(value);
-        await UpdateMediaBufferAsync();
+
+        // Async updates
+        await Task.WhenAll(
+            AddToRecent(value?.Source),
+            _transportControlsService.UpdateTransportControlsDisplayAsync(value),
+            UpdateMediaBufferAsync()
+        );
     }
 
     partial void OnRepeatModeChanged(MediaPlaybackAutoRepeatMode value)
@@ -422,6 +408,27 @@ public sealed partial class MediaListViewModel : ObservableRecipient,
     #endregion
 
     #region Private Methods
+
+    private async Task AddToRecent(object? source)
+    {
+        try
+        {
+            switch (source)
+            {
+                case StorageFile file:
+                    _filesService.AddToRecent(file);
+                    break;
+                case Uri { IsFile: true, IsLoopback: true, IsAbsoluteUri: true } uri:
+                    var fileFromPath = await StorageFile.GetFileFromPathAsync(uri.OriginalString);
+                    _filesService.AddToRecent(fileFromPath);
+                    break;
+            }
+        }
+        catch
+        {
+            // ignored
+        }
+    }
 
     private async Task<Playlist> EnqueueNeighboringFilesAsync(Playlist playlist, StorageFileQueryResult neighboringFilesQuery)
     {
