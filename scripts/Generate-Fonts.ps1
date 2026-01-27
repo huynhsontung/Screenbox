7f864b76b63672cad25381f1a99e4341eb44cc04
United600 <United600@users.noreply.github.com>
2026-01-27 22:38:40 +0000

    add publish option and help to font gen script

--- scripts/Generate-Fonts.ps1
+++ scripts/Generate-Fonts.ps1
@@ -1,8 +1,34 @@
+<#
+.SYNOPSIS
+    Generate TrueType (.ttf) fonts from UFO packages using FontForge.
+.DESCRIPTION
+    Lightweight wrapper that invokes `assets/fonts/fontforge-ufo-to-ttf.pe`
+    to generate .ttf files.
+.PARAMETER Publish
+    (Optional) Specifies whether generated .ttf files should be moved from the
+    source assets folder into the application assets folder. If not specified,
+    generated .ttf files remain in the source assets folder.
+.EXAMPLE
+    PS> .\scripts\Generate-Fonts.ps1
+.EXAMPLE
+    PS> .\scripts\Generate-Fonts.ps1 -Publish
+.NOTES
+    FontForge must be installed and included on the system PATH environment
+    variables.
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$false)]
+    [switch]
+    $Publish
+)
+
 $repositoryPath = Split-Path -Parent $PSScriptRoot
 $fontsPath = Join-Path -Path $repositoryPath -ChildPath "assets\fonts"
 $scriptPath = Join-Path -Path $repositoryPath -ChildPath "assets\fonts\fontforge-ufo-to-ttf.pe"
+$publishPath = Join-Path -Path $repositoryPath -ChildPath "Screenbox\Assets\Fonts"
 
-# Check if FontForge is installed.
 if (-not (Get-Command "fontforge" -ErrorAction SilentlyContinue)) {
     Write-Error "FontForge is not installed or not in PATH environment variable."
     exit 1
@@ -16,7 +42,19 @@ if ($ufoDirs.Count -gt 0) {
     # Build argument list: -script <scriptFile> <ufo#1> <ufo#2> ...
     $cmdArgs = @('-script', $scriptPath) + $ufoDirs
     & "fontforge" @cmdArgs
+
+    if ($Publish) {
+        try {
+            Move-Item -Path (Join-Path $fontsPath '*.ttf') -Destination $publishPath -Force -ErrorAction Stop
+        } catch [System.Management.Automation.ItemNotFoundException] {
+            Write-Host "No .ttf files found in the folder $fontsPath."
+        } catch {
+            Write-Error $_.Exception.Message
+            exit 1
+        }
+    }
+
 } else {
-    Write-Host "No UFO packages found in the folder $fontsPath"
+    Write-Host "No UFO packages found in the folder $fontsPath."
     exit 0
 }
