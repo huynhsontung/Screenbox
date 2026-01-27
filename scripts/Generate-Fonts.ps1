916cec2f40395bd0f7d9b24e967ee713b2758806
United600 <United600@users.noreply.github.com>
2026-01-27 21:24:47 +0000

    refactor font gen scripts for dynamic font discovery

--- scripts/Generate-Fonts.ps1
+++ scripts/Generate-Fonts.ps1
@@ -1,6 +1,22 @@
 $repositoryPath = Split-Path -Parent $PSScriptRoot
+$fontsPath = Join-Path -Path $repositoryPath -ChildPath "assets\fonts"
 $scriptPath = Join-Path -Path $repositoryPath -ChildPath "assets\fonts\fontforge-ufo-to-ttf.pe"
-$mdl2PackagePath = Join-Path -Path $repositoryPath -ChildPath "assets\fonts\ScreenboxMDL2Assets.ufo"
-$fluentPackagePath = Join-Path -Path $repositoryPath -ChildPath "assets\fonts\ScreenboxFluentIcons.ufo"
 
-& "fontforge" -script $scriptPath $mdl2PackagePath $fluentPackagePath
+# Check if FontForge is installed.
+if (-not (Get-Command "fontforge" -ErrorAction SilentlyContinue)) {
+    Write-Error "FontForge is not installed or not in PATH environment variable."
+    exit 1
+}
+
+$ufoDirs = @(Get-ChildItem -Path $fontsPath -Filter '*.ufo' -Directory -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName)
+
+if ($ufoDirs.Count -gt 0) {
+    Write-Host "Found UFO packages:`n$($ufoDirs -join "`n")"
+
+    # Build argument list: -script <scriptFile> <ufo#1> <ufo#2> ...
+    $cmdArgs = @('-script', $scriptPath) + $ufoDirs
+    & "fontforge" @cmdArgs
+} else {
+    Write-Host "No UFO packages found in the folder $fontsPath"
+    exit 0
+}
