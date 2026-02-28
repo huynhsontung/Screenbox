548693580605f9aa03fed4fa3b3e1b4c4ac5474d
United600 <United600@users.noreply.github.com>
2026-02-28 14:24:32 +0000

    add equalizer dialog and view models

---
+++ Screenbox/Commands/ShowEqualizerCommand.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+
+using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using Screenbox.Dialogs;
+
+namespace Screenbox.Commands;
+
+internal sealed class ShowEqualizerCommand : IRelayCommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    private readonly AsyncRelayCommand _asyncCommand;
+
+    public ShowEqualizerCommand()
+    {
+        _asyncCommand = new AsyncRelayCommand(ShowDialogAsync);
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return _asyncCommand.CanExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _asyncCommand.Execute(parameter);
+    }
+
+    public void NotifyCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    private async Task ShowDialogAsync()
+    {
+        var dialog = new EqualizerDialog();
+        await dialog.ShowAsync();
+    }
+}
---
+++ Screenbox/Dialogs/EqualizerDialog.xaml.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Screenbox.Core.ViewModels;
+using Screenbox.Helpers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Screenbox.Dialogs;
+
+public sealed partial class EqualizerDialog : ContentDialog
+{
+    internal EqualizerViewModel ViewModel => (EqualizerViewModel)DataContext;
+
+    public EqualizerDialog()
+    {
+        this.InitializeComponent();
+        DataContext = Ioc.Default.GetRequiredService<EqualizerViewModel>();
+        FlowDirection = GlobalizationHelper.GetFlowDirection();
+        RequestedTheme = ((FrameworkElement)Window.Current.Content).RequestedTheme;
+
+        ViewModel.Presets.Add(Strings.Resources.Custom);
+    }
+
+    public static string FormatFrequency(float frequency)
+    {
+        return frequency >= 1000
+            ? $"{frequency / 1000} kHz"
+            : $"{frequency} Hz";
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (VisualTreeHelper.GetChild(this, 0) is Border containerBorder)
+        {
+            if (containerBorder.FindName("Title") is ContentControl titleControl)
+            {
+                titleControl.HorizontalAlignment = HorizontalAlignment.Stretch;
+                titleControl.HorizontalContentAlignment = HorizontalAlignment.Stretch;
+            }
+        }
+    }
+}
