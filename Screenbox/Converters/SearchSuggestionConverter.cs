85776528f81f1c02895676b05049ddfd6c7afeff
United600 <United600@users.noreply.github.com>
2026-05-15 22:17:52 +0100

    improve search suggestion model and related logic

--- Screenbox/Converters/SearchSuggestionConverter.cs
+++ Screenbox/Converters/SearchSuggestionConverter.cs
@@ -6,38 +6,41 @@ namespace Screenbox.Converters;
 public sealed class SearchSuggestionConverter
 {
     /// <summary>
-    /// Gets the search suggestion name based on the specified suggestion kind.
+    /// Gets the search suggestion name based on the specified suggestion type.
     /// </summary>
-    /// <param name="name">The query text of the current search.</param>
-    /// <param name="type">The suggestion category of the current search.</param>
+    /// <param name="text">The display text of the suggestion.</param>
+    /// <param name="type">The category of the suggestion.</param>
     /// <returns>
-    /// The query text if <paramref name="type"/> is not <see cref="SearchSuggestionKind.None"/>;
+    /// The query text if <paramref name="type"/> is not <see cref="SearchSuggestionType.None"/>;
     /// otherwise, a localized "no results" message containing the query string.
     /// </returns>
-    public static string GetName(string name, SearchSuggestionKind? type)
+    public static string GetName(SearchSuggestionType type, string text)
     {
-        return type == SearchSuggestionKind.None ? Strings.Resources.SearchNoResults(name) : name;
+        return type is not SearchSuggestionType.None
+            ? text
+            : Strings.Resources.SearchNoResults(text);
     }
 
     /// <summary>
-    /// Gets the screen reader search suggestion name based on the specified suggestion kind.
+    /// Gets the screen reader search suggestion name based on the specified
+    /// suggestion type.
     /// </summary>
-    /// <param name="name">The query text of the current search.</param>
-    /// <param name="type">The suggestion category of the current search.</param>
+    /// <param name="text">The query text of the current search.</param>
+    /// <param name="type">The category of the suggestion.</param>
     /// <returns>
-    /// The query text prefixed with the appropriate category label or
-    /// a "no results" message if <paramref name="type"/> is <see cref="SearchSuggestionKind.None"/>.
+    /// The query text prefixed with the appropriate category label or a "no results"
+    /// message if <paramref name="type"/> is <see cref="SearchSuggestionType.None"/>.
     /// </returns>
-    public static string GetAutomationName(string name, SearchSuggestionKind? type)
+    public static string GetAutomationName(SearchSuggestionType type, string text)
     {
         return type switch
         {
-            SearchSuggestionKind.None => Strings.Resources.SearchNoResults(name),
-            SearchSuggestionKind.Song => $"{Strings.Resources.Song} {name}",
-            SearchSuggestionKind.Album => $"{Strings.Resources.PropertyAlbum} {name}",
-            SearchSuggestionKind.Artist => $"{Strings.Resources.Artist} {name}",
-            SearchSuggestionKind.Video => $"{Strings.Resources.Video} {name}",
-            _ => name
+            SearchSuggestionType.None => Strings.Resources.SearchNoResults(text),
+            SearchSuggestionType.Song => $"{Strings.Resources.Song} {text}",
+            SearchSuggestionType.Album => $"{Strings.Resources.PropertyAlbum} {text}",
+            SearchSuggestionType.Artist => $"{Strings.Resources.Artist} {text}",
+            SearchSuggestionType.Video => $"{Strings.Resources.Video} {text}",
+            _ => text,
         };
     }
 
@@ -46,10 +49,10 @@ public sealed class SearchSuggestionConverter
     /// </summary>
     /// <param name="type">The suggestion category of the current search.</param>
     /// <returns>
-    /// <see cref="Visibility.Collapsed"/> if <paramref name="type"/> is <see cref="SearchSuggestionKind.None"/>;
+    /// <see cref="Visibility.Collapsed"/> if <paramref name="type"/> is <see cref="SearchSuggestionType.None"/>;
     /// otherwise, <see cref="Visibility.Visible"/>.</returns>
-    public static Visibility GetGlyphVisibility(SearchSuggestionKind? type)
+    public static Visibility GetGlyphVisibility(SearchSuggestionType type)
     {
-        return type == SearchSuggestionKind.None ? Visibility.Collapsed : Visibility.Visible;
+        return type is SearchSuggestionType.None ? Visibility.Collapsed : Visibility.Visible;
     }
 }
