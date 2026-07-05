e170a7716233ace7a776a20ce053553087307cf9
Tung Huynh <31434093+huynhsontung@users.noreply.github.com>
2026-07-04 17:16:55 -0700

    remove AppCenter usage

--- Screenbox/App.xaml.cs
+++ Screenbox/App.xaml.cs
@@ -3,15 +3,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.ExceptionServices;
 using System.Security;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Messaging;
 using CommunityToolkit.WinUI.Helpers;
 using LibVLCSharp.Shared;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
 using Microsoft.Extensions.DependencyInjection;
 using Screenbox.Core;
 using Screenbox.Core.Helpers;
@@ -136,7 +132,7 @@ sealed partial class App : Application
 
     private static void ConfigureAppCenter()
     {
-        AppCenter.Start(Secrets.AppCenterApiKey, typeof(Analytics), typeof(Crashes));
+        //AppCenter.Start(Secrets.AppCenterApiKey, typeof(Analytics), typeof(Crashes));
     }
 
     private void ConfigureSentry()
