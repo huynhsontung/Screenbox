e1cd9fadd77bd02afb2d1bdd280782926c864fb1
Tung Huynh <31434093+huynhsontung@users.noreply.github.com>
2026-07-14 00:49:34 -0700

    make lively JSON serialization native aot compatible

--- Screenbox.Lively/Models/Serialization/LivelyJsonContext.cs
+++ Screenbox.Lively/Models/Serialization/LivelyJsonContext.cs
@@ -5,6 +5,7 @@ namespace Screenbox.Lively.Models.Serialization;
 [JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Metadata)]
 [JsonSerializable(typeof(LivelyInfoModel))]
 [JsonSerializable(typeof(LivelyMusicModel))]
+[JsonSerializable(typeof(LivelyPlaybackStateModel))]
 internal sealed partial class LivelyJsonContext : JsonSerializerContext
 {
 }
