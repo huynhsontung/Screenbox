12f205a2ee6d90e2d97c467ebba899cb7694eeda
United600 <United600@users.noreply.github.com>
2026-07-28 21:48:28 +0100

    update NotificationView.xaml.cs to use the file‑scoped namespace

--- Screenbox/Controls/NotificationView.xaml.cs
+++ Screenbox/Controls/NotificationView.xaml.cs
@@ -10,103 +10,102 @@ using Windows.UI.Xaml.Controls.Primitives;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace Screenbox.Controls
+namespace Screenbox.Controls;
+
+public sealed partial class NotificationView : UserControl
 {
-    public sealed partial class NotificationView : UserControl
+    private NotificationViewModel ViewModel => (NotificationViewModel)DataContext;
+
+    public NotificationView()
     {
-        private NotificationViewModel ViewModel => (NotificationViewModel)DataContext;
+        this.InitializeComponent();
+        DataContext = Ioc.Default.GetRequiredService<NotificationViewModel>();
+    }
 
-        public NotificationView()
+    private ButtonBase? GetActionButton(NotificationKind kind, string? actionContent, RelayCommand? actionCommand)
+    {
+        if (actionCommand is null)
         {
-            this.InitializeComponent();
-            DataContext = Ioc.Default.GetRequiredService<NotificationViewModel>();
+            return null;
         }
 
-        private ButtonBase? GetActionButton(NotificationKind kind, string? actionContent, RelayCommand? actionCommand)
+        return kind switch
         {
-            if (actionCommand is null)
+            NotificationKind.AccessDeniedMusicLibrary or
+            NotificationKind.AccessDeniedPicturesLibrary or
+            NotificationKind.AccessDeniedVideosLibrary => new HyperlinkButton
             {
-                return null;
-            }
-
-            return kind switch
+                Content = Strings.Resources.OpenPrivacySettingsButtonText,
+                Command = actionCommand,
+            },
+            NotificationKind.FrameSaved => new HyperlinkButton
             {
-                NotificationKind.AccessDeniedMusicLibrary or
-                NotificationKind.AccessDeniedPicturesLibrary or
-                NotificationKind.AccessDeniedVideosLibrary => new HyperlinkButton
-                {
-                    Content = Strings.Resources.OpenPrivacySettingsButtonText,
-                    Command = actionCommand,
-                },
-                NotificationKind.FrameSaved => new HyperlinkButton
-                {
-                    Content = actionContent,
-                    Command = actionCommand,
-                },
-                NotificationKind.ResumePosition => new Button
-                {
-                    Content = Strings.Resources.GoToPosition(actionContent),
-                    Command = actionCommand,
-                },
-                _ => null
-            };
-        }
+                Content = actionContent,
+                Command = actionCommand,
+            },
+            NotificationKind.ResumePosition => new Button
+            {
+                Content = Strings.Resources.GoToPosition(actionContent),
+                Command = actionCommand,
+            },
+            _ => null
+        };
+    }
 
-        private string? GetDisplayTitle(NotificationKind kind, string? title, int count)
+    private string? GetDisplayTitle(NotificationKind kind, string? title, int count)
+    {
+        return kind switch
         {
-            return kind switch
-            {
-                NotificationKind.None => null,
-                NotificationKind.Generic => title,
-                NotificationKind.AccessDeniedMusicLibrary => Strings.Resources.AccessDeniedMusicLibraryTitle,
-                NotificationKind.AccessDeniedPicturesLibrary => Strings.Resources.AccessDeniedPicturesLibraryTitle,
-                NotificationKind.AccessDeniedVideosLibrary => Strings.Resources.AccessDeniedVideosLibraryTitle,
-                NotificationKind.FailedToAddFolder => Strings.Resources.FailedToAddFolderNotificationTitle,
-                NotificationKind.FailedToInitialize => Strings.Resources.FailedToInitializeNotificationTitle,
-                NotificationKind.FailedToLoadMedia => Strings.Resources.FailedToLoadMediaNotificationTitle,
-                NotificationKind.FailedToLoadSubtitle => Strings.Resources.FailedToLoadSubtitleNotificationTitle,
-                NotificationKind.FailedToOpenFiles => Strings.Resources.FailedToOpenFilesNotificationTitle,
-                NotificationKind.FailedToSaveFrame => Strings.Resources.FailedToSaveFrameNotificationTitle,
-                NotificationKind.FrameSaved => Strings.Resources.FrameSavedNotificationTitle,
-                NotificationKind.PlaylistCreated => Strings.Resources.PlaylistCreatedNotificationTitle(title ?? string.Empty),
-                NotificationKind.PlaylistDeleted => Strings.Resources.PlaylistDeletedNotificationTitle(title ?? string.Empty),
-                NotificationKind.PlaylistRenamed => Strings.Resources.PlaylistRenamedNotificationTitle(title ?? string.Empty),
-                NotificationKind.PlaylistItemsAdded => Strings.Resources.PlaylistItemsAddedNotificationTitle(count, title ?? string.Empty),
-                NotificationKind.ResumePosition => Strings.Resources.ResumePositionNotificationTitle,
-                NotificationKind.SubtitleAdded => Strings.Resources.SubtitleAddedNotificationTitle,
-                _ => null,
-            };
-        }
+            NotificationKind.None => null,
+            NotificationKind.Generic => title,
+            NotificationKind.AccessDeniedMusicLibrary => Strings.Resources.AccessDeniedMusicLibraryTitle,
+            NotificationKind.AccessDeniedPicturesLibrary => Strings.Resources.AccessDeniedPicturesLibraryTitle,
+            NotificationKind.AccessDeniedVideosLibrary => Strings.Resources.AccessDeniedVideosLibraryTitle,
+            NotificationKind.FailedToAddFolder => Strings.Resources.FailedToAddFolderNotificationTitle,
+            NotificationKind.FailedToInitialize => Strings.Resources.FailedToInitializeNotificationTitle,
+            NotificationKind.FailedToLoadMedia => Strings.Resources.FailedToLoadMediaNotificationTitle,
+            NotificationKind.FailedToLoadSubtitle => Strings.Resources.FailedToLoadSubtitleNotificationTitle,
+            NotificationKind.FailedToOpenFiles => Strings.Resources.FailedToOpenFilesNotificationTitle,
+            NotificationKind.FailedToSaveFrame => Strings.Resources.FailedToSaveFrameNotificationTitle,
+            NotificationKind.FrameSaved => Strings.Resources.FrameSavedNotificationTitle,
+            NotificationKind.PlaylistCreated => Strings.Resources.PlaylistCreatedNotificationTitle(title ?? string.Empty),
+            NotificationKind.PlaylistDeleted => Strings.Resources.PlaylistDeletedNotificationTitle(title ?? string.Empty),
+            NotificationKind.PlaylistRenamed => Strings.Resources.PlaylistRenamedNotificationTitle(title ?? string.Empty),
+            NotificationKind.PlaylistItemsAdded => Strings.Resources.PlaylistItemsAddedNotificationTitle(count, title ?? string.Empty),
+            NotificationKind.ResumePosition => Strings.Resources.ResumePositionNotificationTitle,
+            NotificationKind.SubtitleAdded => Strings.Resources.SubtitleAddedNotificationTitle,
+            _ => null,
+        };
+    }
 
-        private string? GetDisplayMessage(NotificationKind kind, string? message)
+    private string? GetDisplayMessage(NotificationKind kind, string? message)
+    {
+        return kind switch
         {
-            return kind switch
-            {
-                NotificationKind.None => null,
-                NotificationKind.Generic => message,
-                NotificationKind.AccessDeniedMusicLibrary => Strings.Resources.AccessDeniedMessage,
-                NotificationKind.AccessDeniedPicturesLibrary => Strings.Resources.AccessDeniedMessage,
-                NotificationKind.AccessDeniedVideosLibrary => Strings.Resources.AccessDeniedMessage,
-                NotificationKind.FailedToAddFolder => message,
-                NotificationKind.FailedToInitialize => message,
-                NotificationKind.FailedToLoadMedia => message,
-                NotificationKind.FailedToLoadSubtitle => message,
-                NotificationKind.FailedToOpenFiles => message,
-                NotificationKind.FailedToSaveFrame => message,
-                NotificationKind.SubtitleAdded => message,
-                _ => null,
-            };
-        }
+            NotificationKind.None => null,
+            NotificationKind.Generic => message,
+            NotificationKind.AccessDeniedMusicLibrary => Strings.Resources.AccessDeniedMessage,
+            NotificationKind.AccessDeniedPicturesLibrary => Strings.Resources.AccessDeniedMessage,
+            NotificationKind.AccessDeniedVideosLibrary => Strings.Resources.AccessDeniedMessage,
+            NotificationKind.FailedToAddFolder => message,
+            NotificationKind.FailedToInitialize => message,
+            NotificationKind.FailedToLoadMedia => message,
+            NotificationKind.FailedToLoadSubtitle => message,
+            NotificationKind.FailedToOpenFiles => message,
+            NotificationKind.FailedToSaveFrame => message,
+            NotificationKind.SubtitleAdded => message,
+            _ => null,
+        };
+    }
 
-        private InfoBarSeverity ConvertInfoBarSeverity(NotificationLevel level)
+    private InfoBarSeverity ConvertInfoBarSeverity(NotificationLevel level)
+    {
+        return level switch
         {
-            return level switch
-            {
-                NotificationLevel.Error => InfoBarSeverity.Error,
-                NotificationLevel.Warning => InfoBarSeverity.Warning,
-                NotificationLevel.Success => InfoBarSeverity.Success,
-                _ => InfoBarSeverity.Informational
-            };
-        }
+            NotificationLevel.Error => InfoBarSeverity.Error,
+            NotificationLevel.Warning => InfoBarSeverity.Warning,
+            NotificationLevel.Success => InfoBarSeverity.Success,
+            _ => InfoBarSeverity.Informational
+        };
     }
 }
