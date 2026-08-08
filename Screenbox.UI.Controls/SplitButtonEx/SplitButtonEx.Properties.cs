867e2dd23687e1505429d6e054d8dcfa76e1d8a2
United600 <United600@users.noreply.github.com>
2026-08-08 21:08:45 +0100

    add SpliButtonEx control
    refactor split button usage to the new control
    
    remove SplitButtonXYNavigation behavior
    
    remove SplitButton extensions

---
+++ Screenbox.UI.Controls/SplitButtonEx/SplitButtonEx.Properties.cs
@@ -0,0 +1,111 @@
+using Windows.UI.Xaml;
+
+namespace Screenbox.UI.Controls;
+
+public sealed partial class SplitButtonEx
+{
+    /// <summary>
+    /// Identifies the <see cref="PrimaryButtonAccessKey"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty PrimaryButtonAccessKeyProperty = DependencyProperty.Register(
+        nameof(PrimaryButtonAccessKey),
+        typeof(string),
+        typeof(SplitButtonEx),
+        new PropertyMetadata(null, OnPropertyChanged));
+
+    /// <summary>
+    /// Gets or sets the access key (mnemonic) for the primary button part.
+    /// </summary>
+    /// <value>The access key (mnemonic) for the primary button part.</value>
+    public string? PrimaryButtonAccessKey
+    {
+        get { return (string?)GetValue(PrimaryButtonAccessKeyProperty); }
+        set { SetValue(PrimaryButtonAccessKeyProperty, value); }
+    }
+
+    /// <summary>
+    /// Identifies the <see cref="PrimaryButtonToolTip"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty PrimaryButtonToolTipProperty = DependencyProperty.Register(
+        nameof(PrimaryButtonToolTip),
+        typeof(object),
+        typeof(SplitButtonEx),
+        new PropertyMetadata(null, OnPropertyChanged));
+
+    /// <summary>
+    /// Gets or sets the object or string content of the primary button part ToolTip.
+    /// </summary>
+    /// <value>The object's tooltip content. The default is <see langword="null"/></value>
+    public object? PrimaryButtonToolTip
+    {
+        get { return (object?)GetValue(PrimaryButtonToolTipProperty); }
+        set { SetValue(PrimaryButtonToolTipProperty, value); }
+    }
+
+    /// <summary>
+    /// Identifies the <see cref="SecondaryButtonAccessKey"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty SecondaryButtonAccessKeyProperty = DependencyProperty.Register(
+        nameof(SecondaryButtonAccessKey),
+        typeof(string),
+        typeof(SplitButtonEx),
+        new PropertyMetadata(null, OnPropertyChanged));
+
+    /// <summary>
+    /// Gets or sets the access key (mnemonic) for the secondary button part.
+    /// </summary>
+    /// <value>The access key (mnemonic) for the secondary button part.</value>
+    public string? SecondaryButtonAccessKey
+    {
+        get { return (string?)GetValue(SecondaryButtonAccessKeyProperty); }
+        set { SetValue(SecondaryButtonAccessKeyProperty, value); }
+    }
+
+    /// <summary>
+    /// Identifies the <see cref="SecondaryButtonToolTip"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty SecondaryButtonToolTipProperty = DependencyProperty.Register(
+        nameof(SecondaryButtonToolTip),
+        typeof(object),
+        typeof(SplitButtonEx),
+        new PropertyMetadata(null, OnPropertyChanged));
+
+    /// <summary>
+    /// Gets or sets the object or string content of the secondary button part ToolTip.
+    /// </summary>
+    /// <value>The object's tooltip content. The default is <see langword="null"/></value>
+    public object? SecondaryButtonToolTip
+    {
+        get { return (object?)GetValue(SecondaryButtonToolTipProperty); }
+        set { SetValue(SecondaryButtonToolTipProperty, value); }
+    }
+
+    /// <summary>
+    /// Identifies the <see cref="IsSecondaryButtonTabStop"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty IsSecondaryButtonTabStopProperty = DependencyProperty.Register(
+        nameof(IsSecondaryButtonTabStop),
+        typeof(bool),
+        typeof(SplitButtonEx),
+        new PropertyMetadata(false, OnPropertyChanged));
+
+    /// <summary>
+    /// Gets or sets a value that indicates whether the secondary button part
+    /// is included in tab navigation.
+    /// </summary>
+    /// <value>
+    /// <see langword="true"/> if the part is included in tab navigation;
+    /// otherwise, <see langword="false"/>. The default is <see langword="false"/>.
+    /// </value>
+    public bool IsSecondaryButtonTabStop
+    {
+        get { return (bool)GetValue(IsSecondaryButtonTabStopProperty); }
+        set { SetValue(IsSecondaryButtonTabStopProperty, value); }
+    }
+
+    private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var owner = (SplitButtonEx)d;
+        owner.OnPropertyChanged(e);
+    }
+}
---
+++ Screenbox.UI.Controls/SplitButtonEx/SplitButtonEx.cs
@@ -0,0 +1,202 @@
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+
+using SplitButton = Microsoft.UI.Xaml.Controls.SplitButton;
+
+namespace Screenbox.UI.Controls;
+
+/// <summary>
+/// Represents an extended <see cref='SplitButton'/> control that provides a convenient way
+/// to set independent access keys and tooltips for the primary and secondary button parts.
+/// </summary>
+/// <remarks>
+/// You can modify the default XY focus navigation behavior using the <see cref="IsSecondaryButtonTabStop"/>
+/// property to enable keyboard and game controller navigation to the secondary button part.
+/// </remarks>
+/// <example>
+/// The following example shows how to create a <see cref="SplitButtonEx"/> with separate tool tips
+/// for the primary and secondary button parts, and navigation to the secondary button part enabled.
+/// <code lang="xml"><![CDATA[
+/// <local:SplitButtonEx Flyout="{StaticResource BrushFlyout}"
+///                      IsSecondaryButtonTabStop="True"
+///                      PrimaryButtonToolTip="Foreground color">
+///     <local:SplitButtonEx.SecondaryButtonToolTip>
+///         <ToolTip Content="Color options" />
+///     </local:SplitButtonEx.SecondaryButtonToolTip>
+///     <Border x:Name="SelectedColorBorder" Width="20" Height="20"/>
+/// </local:SplitButtonEx>
+/// ]]></code>
+/// </example>
+public sealed partial class SplitButtonEx : SplitButton
+{
+    private const string PrimaryButtonName = "PrimaryButton";
+    private const string SecondaryButtonName = "SecondaryButton";
+
+    private readonly long _toolTipPlacementCallbackToken;
+
+    private Button? _primaryButton;
+    private Button? _secondaryButton;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SplitButtonEx"/> class.
+    /// </summary>
+    public SplitButtonEx()
+    {
+        DefaultStyleKey = typeof(SplitButton);
+
+        _toolTipPlacementCallbackToken = RegisterPropertyChangedCallback(ToolTipService.PlacementProperty, OnToolTipServicePlacementChanged);
+    }
+
+    protected override void OnApplyTemplate()
+    {
+        _primaryButton = null;
+        _secondaryButton = null;
+
+        base.OnApplyTemplate();
+
+        if (GetTemplateChild(PrimaryButtonName) is Button primaryButton)
+        {
+            _primaryButton = primaryButton;
+
+            UpdatePrimaryButtonAccessKey();
+            UpdatePrimaryButtonToolTip();
+            UpdateToolTipPlacement(primaryButton);
+        }
+
+        if (GetTemplateChild(SecondaryButtonName) is Button secondaryButton)
+        {
+            _secondaryButton = secondaryButton;
+
+            UpdateSecondaryButtonAccessKey();
+            UpdateSecondaryButtonToolTip();
+            UpdateToolTipPlacement(secondaryButton);
+            UpdateIsSecondaryButtonTabStop();
+        }
+    }
+
+    private void OnToolTipServicePlacementChanged(DependencyObject sender, DependencyProperty dp)
+    {
+        if (_primaryButton is not null && PrimaryButtonToolTip is not null)
+        {
+            UpdateToolTipPlacement(_primaryButton);
+        }
+
+        if (_secondaryButton is not null && SecondaryButtonToolTip is not null)
+        {
+            UpdateToolTipPlacement(_secondaryButton);
+        }
+    }
+
+    private void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+    {
+        var property = e.Property;
+
+        if (property == PrimaryButtonAccessKeyProperty)
+        {
+            UpdatePrimaryButtonAccessKey();
+        }
+        else if (property == PrimaryButtonToolTipProperty)
+        {
+            UpdatePrimaryButtonToolTip();
+        }
+        else if (property == SecondaryButtonAccessKeyProperty)
+        {
+            UpdateSecondaryButtonAccessKey();
+        }
+        else if (property == SecondaryButtonToolTipProperty)
+        {
+            UpdateSecondaryButtonToolTip();
+        }
+        else if (property == IsSecondaryButtonTabStopProperty)
+        {
+            UpdateIsSecondaryButtonTabStop();
+        }
+    }
+
+    private void UpdatePrimaryButtonAccessKey()
+    {
+        string? accessKey = PrimaryButtonAccessKey;
+
+        if (_primaryButton is null || string.IsNullOrEmpty(accessKey))
+            return;
+
+        _primaryButton.AccessKey = accessKey;
+    }
+
+    private void UpdatePrimaryButtonToolTip()
+    {
+        var primaryToolTip = PrimaryButtonToolTip;
+
+        if (_primaryButton is null || primaryToolTip is null)
+            return;
+
+        ToolTipService.SetToolTip(_primaryButton, primaryToolTip);
+    }
+
+    private void UpdateSecondaryButtonAccessKey()
+    {
+        string? accessKey = SecondaryButtonAccessKey;
+
+        if (_secondaryButton is null || string.IsNullOrEmpty(accessKey))
+            return;
+
+        _secondaryButton.AccessKey = accessKey;
+        _secondaryButton.ExitDisplayModeOnAccessKeyInvoked = false;
+
+        // Prevent the key tips from overlapping.
+        if (KeyTipPlacementMode == KeyTipPlacementMode.Right
+            && !string.IsNullOrEmpty(accessKey))
+        {
+            _secondaryButton.KeyTipPlacementMode = KeyTipPlacementMode.Auto;
+        }
+    }
+
+    private void UpdateSecondaryButtonToolTip()
+    {
+        var secondaryToolTip = SecondaryButtonToolTip;
+
+        if (_secondaryButton is null || secondaryToolTip is null)
+            return;
+
+        ToolTipService.SetToolTip(_secondaryButton, secondaryToolTip);
+
+        switch (secondaryToolTip)
+        {
+            case string text:
+                AutomationProperties.SetName(_secondaryButton, text);
+                AutomationProperties.SetAccessibilityView(_secondaryButton, AccessibilityView.Content);
+                break;
+            case ToolTip toolTip when toolTip.Content is string content:
+                AutomationProperties.SetName(_secondaryButton, content);
+                AutomationProperties.SetAccessibilityView(_secondaryButton, AccessibilityView.Content);
+                break;
+        }
+    }
+
+    private void UpdateIsSecondaryButtonTabStop()
+    {
+        if (_secondaryButton is null || !IsSecondaryButtonTabStop)
+            return;
+
+        _secondaryButton.IsTabStop = true;
+        _secondaryButton.XYFocusLeftNavigationStrategy = XYFocusNavigationStrategy.NavigationDirectionDistance;
+
+        // Update the appearance of the secondary button part to match the focus visual of the control.
+        var radius = CornerRadius;
+        _secondaryButton.CornerRadius = new CornerRadius(0, radius.TopRight, radius.BottomRight, 0);
+        _secondaryButton.FocusVisualMargin = FocusVisualMargin;
+    }
+
+    private void UpdateToolTipPlacement(Button button)
+    {
+        var placementMode = ToolTipService.GetPlacement(this);
+        if (placementMode != PlacementMode.Top)
+        {
+            ToolTipService.SetPlacement(button, placementMode);
+        }
+    }
+}
