885ea9ce7f5c8997f9fcfae319e7e002a4a08364
United600 <United600@users.noreply.github.com>
2026-09-12 16:00:17 +0100

    minor tweaks to main ci action and update-manifest script

--- scripts/update-manifest.ps1
+++ scripts/update-manifest.ps1
@@ -1,34 +1,43 @@
 [CmdletBinding()]
 param (
-  [Parameter(Mandatory=$false)]
-  [ValidatePattern('^\d+\.\d+\.\d+$')]
-  [string]
-  $Version
+    [Parameter(Mandatory=$false)]
+    [ValidatePattern('^\d+\.\d+\.\d+$')]
+    [string]$Version
 )
+
+New-Variable -Name IdentityName -Value "18496Starpine.Screenbox" -Option Constant
+New-Variable -Name IdentityPublisher -Value 'CN=ABCDF790-DBE4-48F7-8204-32FCB69ADF9C' -Option Constant
+New-Variable -Name IdentityPublisherUnsigned -Value "$IdentityPublisher, OID.2.25.311729368913984317654407730594956997722=1" -Option Constant
+
+New-Variable -Name DisplayName -Value "Screenbox" -Option Constant
+New-Variable -Name VisualDisplayName -Value "ms-resource:ManifestResources/AppDisplayName" -Option Constant
 
 $repoPath = Split-Path -Parent $PSScriptRoot
 $manifestPath = Join-Path -Path $repoPath -ChildPath "Screenbox/Package.appxmanifest"
 
 [xml]$xmlDoc = Get-Content -Path $manifestPath
-$xmlDoc.Package.Identity.Name="18496Starpine.Screenbox"
-$xmlDoc.Package.Properties.DisplayName="Screenbox"
-$xmlDoc.Package.Applications.Application.VisualElements.DisplayName="ms-resource:ManifestResources/AppDisplayName"
-
-$publisher = 'CN=ABCDF790-DBE4-48F7-8204-32FCB69ADF9C'
-$publisherUnsigned = "$publisher, OID.2.25.311729368913984317654407730594956997722=1"
+$xmlDoc.Package.Identity.Name = $IdentityName
 
 if ($PSBoundParameters.ContainsKey('Version')) {
-  $xmlDoc.Package.Identity.Publisher = $publisher
-  $xmlDoc.Package.Identity.Version = $Version + ".0"
+    $xmlDoc.Package.Identity.Publisher = $IdentityPublisher
+    $xmlDoc.Package.Identity.Version = "$Version.0"
 }
 else {
-  $xmlDoc.Package.Identity.Publisher = $publisherUnsigned
+    $xmlDoc.Package.Identity.Publisher = $IdentityPublisherUnsigned
 }
+
+$xmlDoc.Package.Properties.DisplayName = $DisplayName
+$xmlDoc.Package.Applications.Application.VisualElements.DisplayName = $VisualDisplayName
 
 $settings = New-Object System.Xml.XmlWriterSettings
 $settings.Encoding = [System.Text.UTF8Encoding]::new($false)
 $settings.Indent = $true
 
 $writer = [System.Xml.XmlWriter]::Create($manifestPath, $settings)
-$xmlDoc.Save($writer)
-$writer.Close()
+
+try {
+    $xmlDoc.Save($writer)
+}
+finally {
+    $writer.Dispose()
+}
